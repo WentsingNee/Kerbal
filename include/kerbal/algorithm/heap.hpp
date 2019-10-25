83a9bfdefb4fd4ed9dd7a4777f5f6a5b05c40233
WentsingNee <1205422966@qq.com>
2019-10-25 09:18:08 +0800

    优化

--- include/kerbal/algorithm/heap.hpp
+++ include/kerbal/algorithm/heap.hpp
@@ -246,82 +246,7 @@ namespace kerbal
 				kerbal::algorithm::push_heap(first, current_adjust);
 				++current_adjust;
 			}
-
-			/*
-			typedef BidirectionalIterator iterator;
-			typedef typename kerbal::iterator::iterator_traits<iterator>::difference_type difference_type;
-
-			if (first == last) {
-				return;
-			}
-
-			std::pair<iterator, difference_type> p(kerbal::iterator::midden_iterator_with_distance(first, last));
-			iterator & i = p.first;
-			difference_type & len = p.second;
-
-			iterator left_son(last); //left son;
-			if ((len & 1) == 0) { // len is even, the last one is left son
-				--i;
-				--left_son;
-				if (pred(*i, *left_son)) {
-					std::iter_swap(i, left_son);
-				}
-				if (i == first) {
-					return;
-				}
-			}
-			iterator right_son(kerbal::iterator::prev(left_son)); //right son;
-			left_son = kerbal::iterator::prev(right_son);
-
-			do {
-				--i;
-				printf("%d %d %d\n", *i, *left_son, *right_son);
-				if (pred(*i, *left_son)) {
-					std::iter_swap(i, static_cast<bool>(pred(*left_son, *right_son)) ? right_son : left_son);
-				} else if (pred(*i, *right_son)) {
-					std::iter_swap(i, right_son);
-				}
-				right_son = kerbal::iterator::prev(left_son);
-				left_son = kerbal::iterator::prev(right_son);
-			} while (i != first);
-
-
-			do {
-				--i;
-				if (pred(*i, *left_son)) {
-					std::iter_swap(i, static_cast<bool>(pred(*left_son, *right_son)) ? right_son : left_son);
-				} else {
-					if (pred(*i, *right_son)) {
-						std::iter_swap(i, right_son);
-					}
-				}
-				right_son = kerbal::iterator::prev(left_son);
-				left_son = kerbal::iterator::prev(right_son);
-			} while (i != first);
-
-			iterator left_son(kerbal::iterator::next(first)); //left son;
-				iterator right_son(kerbal::iterator::next(left_son)); //right son;
-			while (left_son != last) {
-				iterator right_son(kerbal::iterator::next(left_son)); //right son;
-				if (right_son == last) {
-					if (pred(*first, *left_son)) {
-						std::iter_swap(first, left_son);
-					}
-					break;
-				}
-				// has right son
-				if (pred(*first, *left_son)) {
-					std::iter_swap(first, static_cast<bool>(pred(*left_son, *right_son)) ? right_son : left_son);
-				} else {
-					if (pred(*first, *right_son)) {
-						std::iter_swap(first, right_son);
-					}
-				}
-				left_son = kerbal::iterator::next(right_son);
-				++first;
-			}
-			 */
-
+			kerbal::algorithm::push_heap(first, last);
 		}
 
 		template <typename ForwardIterator>
@@ -332,7 +257,6 @@ namespace kerbal
 			typedef typename kerbal::iterator::iterator_traits<iterator>::value_type value_type;
 			kerbal::algorithm::make_heap(first, last, std::less<value_type>());
 		}
-
 
 	} /* namespace algorithm */
 
