67461ca5e675b06d062e0db142d9feb5cc7c929b
WentsingNee <1205422966@qq.com>
2020-08-15 16:32:17 +0800

    新增编译器 builtin 功能的检测工具

---
+++ include/kerbal/config/compiler_private/clang/builtin_detection.hpp
@@ -0,0 +1,29 @@
+/**
+ * @file       builtin_detection.hpp
+ * @brief
+ * @date       2020-07-19
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONFIG_COMPILER_PRIVATE_CLANG_BUILTIN_DETECTION_HPP
+#define KERBAL_CONFIG_COMPILER_PRIVATE_CLANG_BUILTIN_DETECTION_HPP
+
+#include <kerbal/config/compiler_id.hpp>
+
+#if KERBAL_COMPILER_ID != KERBAL_COMPILER_ID_CLANG
+#	error "KERBAL_COMPILER_ID doesn't match requirement!"
+#endif
+
+// https://clang.llvm.org/docs/LanguageExtensions.html#feature-checking-macros
+
+#if defined __has_builtin
+#	define KERBAL_CLANG_PRIVATE_HAS_BUILTIN(x) __has_builtin(x)
+#else
+#	define KERBAL_CLANG_PRIVATE_HAS_BUILTIN(x) 0
+#endif
+
+#endif // KERBAL_CONFIG_COMPILER_PRIVATE_CLANG_BUILTIN_DETECTION_HPP
