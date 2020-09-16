f140e2da8561829eb3b69f4ef97c1b9c8d1b0afe
WentsingNee <1205422966@qq.com>
2020-09-17 00:52:26 +0800

    rotate 支持 forward_iterator 并且提升了速度

--- include/kerbal/algorithm/modifier.hpp
+++ include/kerbal/algorithm/modifier.hpp
@@ -601,52 +601,131 @@ namespace kerbal
 			return kerbal::algorithm::__reverse_copy(first, last, to, kerbal::iterator::iterator_category(first));
 		}
 
+		namespace detail
+		{
 
+			template <typename ForwardIterator>
+			KERBAL_CONSTEXPR14
+			ForwardIterator
+			rotate_swap(ForwardIterator & first, ForwardIterator n_first, ForwardIterator last)
+			{
+				typedef ForwardIterator iterator;
+
+				iterator left(first);
+
+				while (n_first != last) {
+					if (left == first) {
+						first = n_first;
+					}
+					kerbal::algorithm::iter_swap(left, n_first);
+					++left;
+					++n_first;
+				}
+
+				return left;
+			}
+
+			template <typename ForwardIterator>
+			KERBAL_CONSTEXPR14
+			ForwardIterator
+			rotate(ForwardIterator first, ForwardIterator n_first, ForwardIterator last, std::forward_iterator_tag)
+			{
+				typedef ForwardIterator iterator;
+
+				if (n_first == first) {
+					return last;
+				}
+
+				if (n_first == last) {
+					return first;
+				}
+
+				iterator ret(rotate_swap(first, n_first, last));
+
+				n_first = first;
+				first = ret;
+
+				while (n_first != first && n_first != last) {
+					iterator left(rotate_swap(first, n_first, last));
+
+					n_first = first;
+					first = left;
+					// next loop: rotate(left, first, last)
+				}
+				return ret;
+			}
+
+			template <typename RandomAccessIterator>
+			KERBAL_CONSTEXPR14
+			RandomAccessIterator
+			rotate(RandomAccessIterator first, RandomAccessIterator n_first, RandomAccessIterator last,
+					 std::random_access_iterator_tag)
+			{
+				typedef RandomAccessIterator iterator;
+				typedef typename kerbal::iterator::iterator_traits<iterator>::difference_type difference_type;
+				typedef typename kerbal::iterator::iterator_traits<iterator>::value_type value_type;
+
+				difference_type left_len(n_first - first);
+				difference_type right_len(last - n_first);
+
+				iterator ret(first + right_len);
+
+				while (left_len != 0 && right_len != 0) {
+
+//					cout << left_len << "   " << right_len << endl;
+
+					if (left_len <= right_len) {
+						if (left_len == 1) {
+							value_type t(kerbal::compatibility::to_xvalue(*first));
+							*kerbal::algorithm::move(n_first, last, first) = kerbal::compatibility::to_xvalue(t);
+							break;
+						}
+						while (n_first != last) {
+							kerbal::algorithm::iter_swap(first, n_first);
+							++first;
+							++n_first;
+						}
+						right_len = right_len % left_len;
+						left_len = left_len - right_len;
+						n_first = last - right_len;
+						// next loop: rotate(left, n_first', last)
+					} else {
+						if (right_len == 1) {
+							value_type t(kerbal::compatibility::to_xvalue(*n_first));
+							kerbal::algorithm::move_backward(first, n_first, last);
+							*first = kerbal::compatibility::to_xvalue(t);
+							break;
+						}
+						while (n_first != first) {
+							--n_first;
+							--last;
+							kerbal::algorithm::iter_swap(n_first, last);
+						}
+						left_len = left_len % right_len;
+						right_len = right_len - left_len;
+						n_first = first + left_len;
+						// next loop: rotate(first, n_first', right)
+					}
+				}
+
+				return ret;
+			}
+
+		} // namespace detail
 
 		template <typename ForwardIterator>
 		KERBAL_CONSTEXPR14
 		ForwardIterator
-		__send_adv_iterator_to_last(ForwardIterator first, ForwardIterator adv, ForwardIterator last,
-										std::forward_iterator_tag)
+		rotate(ForwardIterator first, ForwardIterator n_first, ForwardIterator last)
 		{
-			while (adv != last) {
-				++first;
-				++adv;
-			}
-			return first;
+			return detail::rotate(first, n_first, last, kerbal::iterator::iterator_category(first));
 		}
 
-		template <typename RandomAccessIterator>
-		KERBAL_CONSTEXPR
-		RandomAccessIterator
-		__send_adv_iterator_to_last(RandomAccessIterator first, RandomAccessIterator adv, RandomAccessIterator last,
-										std::random_access_iterator_tag)
-		{
-			return kerbal::iterator::next(first, kerbal::iterator::distance(adv, last));
-		}
-
-		template <typename RandomAccessIterator>
-		KERBAL_CONSTEXPR
-		RandomAccessIterator
-		__send_adv_iterator_to_last(RandomAccessIterator first, RandomAccessIterator adv, RandomAccessIterator last)
-		{
-			return kerbal::algorithm::__send_adv_iterator_to_last(first, adv, last,
-					kerbal::iterator::iterator_category(first));
-		}
-
-		template <typename BidirectionalIterator>
-		KERBAL_CONSTEXPR14
-		BidirectionalIterator rotate(BidirectionalIterator first, BidirectionalIterator new_first, BidirectionalIterator last)
-		{
-			kerbal::algorithm::reverse(first, new_first);
-			kerbal::algorithm::reverse(new_first, last);
-			kerbal::algorithm::reverse(first, last);
-			return __send_adv_iterator_to_last(first, new_first, last);
-		}
 
 		template <typename ForwardIterator, typename OutputIterator>
 		KERBAL_CONSTEXPR14
-		ForwardIterator rotate_copy(ForwardIterator first, ForwardIterator mid, ForwardIterator last, OutputIterator to)
+		ForwardIterator
+		rotate_copy(ForwardIterator first, ForwardIterator mid, ForwardIterator last, OutputIterator to)
 		{
 			to = kerbal::algorithm::copy(mid, last, to);
 			return kerbal::algorithm::copy(first, mid, to);
