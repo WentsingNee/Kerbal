6231f15e9998cb032931a7d69a74ab24b50f27f2
WentsingNee <1205422966@qq.com>
2021-01-11 21:51:56 +0800

    新增模板测试用例宏

--- include/kerbal/test/test_case.hpp
+++ include/kerbal/test/test_case.hpp
@@ -116,6 +116,11 @@ namespace kerbal
 	void name(kerbal::test::assert_record& record)
 
 
+#define KERBAL_TEMPLATE_TEST_CASE(name, description) \
+	void name(kerbal::test::assert_record& record)
+
+#define KERBAL_TEMPLATE_TEST_CASE_INST(name, description, args...) \
+	static const int KERBAL_JOIN_LINE(__kerbal_test_register_unit_tag) = (kerbal::test::__register_test_suit(#name, name<args>, description), 0);
 
 
 #if __cplusplus >= 201103L
