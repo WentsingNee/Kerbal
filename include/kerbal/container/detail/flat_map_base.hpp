ecf6e286cf05b62eaf19c58f8cd1d1dc3835bbc0
WentsingNee <1205422966@qq.com>
2021-04-01 21:25:32 +0800

    [new-facility] 新增 static_flat_map, static_flat_multimap

---
+++ include/kerbal/container/detail/flat_map_base.hpp
@@ -0,0 +1,653 @@
+/**
+ * @file       flat_map_base.hpp
+ * @brief
+ * @date       2019-12-26
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_DETAIL_FLAT_MAP_BASE_HPP
+#define KERBAL_CONTAINER_DETAIL_FLAT_MAP_BASE_HPP
+
+#include <kerbal/algorithm/binary_search.hpp>
+#include <kerbal/compatibility/move.hpp>
+#include <kerbal/iterator/iterator.hpp>
+#include <kerbal/iterator/iterator_traits.hpp>
+#include <kerbal/type_traits/enable_if.hpp>
+#include <kerbal/utility/compressed_pair.hpp>
+#include <kerbal/utility/throw_this_exception.hpp>
+
+
+namespace kerbal
+{
+
+	namespace container
+	{
+
+		namespace detail
+		{
+
+			template <typename Ordered>
+			class flat_map_common_base
+			{
+				public:
+					typedef typename Ordered::key_compare				key_compare;
+					typedef typename Ordered::key_type					key_type;
+					typedef typename Ordered::value_type::second_type	mapped_type;
+					typedef typename Ordered::value_type				value_type;
+					typedef typename Ordered::const_type				const_type;
+					typedef typename Ordered::reference					reference;
+					typedef typename Ordered::const_reference			const_reference;
+					typedef typename Ordered::pointer					pointer;
+					typedef typename Ordered::const_pointer				const_pointer;
+
+#		if __cplusplus >= 201103L
+					typedef typename Ordered::rvalue_reference				rvalue_reference;
+					typedef typename Ordered::const_rvalue_reference		const_rvalue_reference;
+#		endif
+
+					typedef typename Ordered::size_type						size_type;
+					typedef typename Ordered::difference_type				difference_type;
+
+					typedef typename Ordered::iterator						iterator;
+					typedef typename Ordered::const_iterator				const_iterator;
+					typedef typename Ordered::reverse_iterator				reverse_iterator;
+					typedef typename Ordered::const_reverse_iterator		const_reverse_iterator;
+					typedef typename Ordered::unique_insert_r				unique_insert_r;
+
+				protected:
+
+					Ordered ordered;
+
+					key_compare& key_comp_obj()
+					{
+						return this->ordered.key_comp_obj();
+					}
+
+					const key_compare& key_comp_obj() const
+					{
+						return this->ordered.key_comp_obj();
+					}
+
+				public:
+
+					const key_compare& key_comp() const
+					{
+						return this->ordered.key_comp();
+					}
+
+				protected:
+					flat_map_common_base() :
+							ordered()
+					{
+					}
+
+					explicit flat_map_common_base(key_compare kc) :
+							ordered(kc)
+					{
+					}
+
+				public:
+
+					iterator begin()
+					{
+						return this->ordered.begin();
+					}
+
+					const_iterator begin() const
+					{
+						return this->ordered.begin();
+					}
+
+					iterator end()
+					{
+						return this->ordered.end();
+					}
+
+					const_iterator end() const
+					{
+						return this->ordered.end();
+					}
+
+					const_iterator cbegin() const
+					{
+						return this->ordered.cbegin();
+					}
+
+					const_iterator cend() const
+					{
+						return this->ordered.cend();
+					}
+
+					reverse_iterator rbegin()
+					{
+						return this->ordered.rbegin();
+					}
+
+					const_reverse_iterator rbegin() const
+					{
+						return this->ordered.rbegin();
+					}
+
+					reverse_iterator rend()
+					{
+						return this->ordered.rend();
+					}
+
+					const_reverse_iterator rend() const
+					{
+						return this->ordered.rend();
+					}
+
+					const_reverse_iterator crbegin() const
+					{
+						return this->ordered.crbegin();
+					}
+
+					const_reverse_iterator crend() const
+					{
+						return this->ordered.crend();
+					}
+
+					iterator nth(size_type index)
+					{
+						return this->ordered.nth(index);
+					}
+
+					const_iterator nth(size_type index) const
+					{
+						return this->ordered.nth(index);
+					}
+
+					size_type index_of(const_iterator it) const
+					{
+						return this->ordered.index_of(it);
+					}
+
+					size_type size() const
+					{
+						return this->ordered.size();
+					}
+
+					KERBAL_CONSTEXPR
+					size_type max_size() const KERBAL_NOEXCEPT
+					{
+						return this->ordered.max_size();
+					}
+
+					bool empty() const
+					{
+						return this->ordered.empty();
+					}
+
+					iterator lower_bound(const key_type & key)
+					{
+						return this->ordered.lower_bound(key);
+					}
+
+					const_iterator lower_bound(const key_type & key) const
+					{
+						return this->ordered.lower_bound(key);
+					}
+
+					iterator lower_bound(const key_type & key, const_iterator hint)
+					{
+						return this->ordered.lower_bound(key, hint);
+					}
+
+					const_iterator lower_bound(const key_type & key, const_iterator hint) const
+					{
+						return this->ordered.lower_bound(key, hint);
+					}
+
+					iterator upper_bound(const key_type & key)
+					{
+						return this->ordered.upper_bound(key);
+					}
+
+					const_iterator upper_bound(const key_type & key) const
+					{
+						return this->ordered.upper_bound(key);
+					}
+
+					iterator upper_bound(const key_type & key, const_iterator hint)
+					{
+						return this->ordered.upper_bound(key, hint);
+					}
+
+					const_iterator upper_bound(const key_type & key, const_iterator hint) const
+					{
+						return this->ordered.upper_bound(key, hint);
+					}
+
+					kerbal::utility::compressed_pair<iterator, iterator>
+					equal_range(const key_type & key)
+					{
+						return this->ordered.equal_range(key);
+					}
+
+					kerbal::utility::compressed_pair<const_iterator, const_iterator>
+					equal_range(const key_type & key) const
+					{
+						return this->ordered.equal_range(key);
+					}
+
+					iterator find(const key_type & key)
+					{
+						return this->ordered.find(key);
+					}
+
+					const_iterator find(const key_type & key) const
+					{
+						return this->ordered.find(key);
+					}
+
+					iterator find(const key_type & key, const_iterator hint)
+					{
+						return this->ordered.find(key, hint);
+					}
+
+					const_iterator find(const key_type & key, const_iterator hint) const
+					{
+						return this->ordered.find(key, hint);
+					}
+
+					bool contains(const key_type & key) const
+					{
+						return this->ordered.contains(key);
+					}
+
+					bool contains(const key_type & key, const_iterator hint) const
+					{
+						return this->ordered.contains(key, hint);
+					}
+
+					const_iterator erase(const_iterator pos)
+					{
+						return this->ordered.erase(pos);
+					}
+
+					const_iterator erase(const_iterator first, const_iterator last)
+					{
+						return this->ordered.erase(first, last);
+					}
+
+					void clear()
+					{
+						this->ordered.clear();
+					}
+
+			};
+
+			template <typename Ordered>
+			class flat_map_base: public flat_map_common_base<Ordered>
+			{
+				private:
+					typedef flat_map_common_base<Ordered> super;
+
+				public:
+					typedef typename super::key_compare			key_compare;
+					typedef typename super::key_type			key_type;
+					typedef typename super::mapped_type			mapped_type;
+					typedef typename super::value_type			value_type;
+					typedef typename super::const_type			const_type;
+					typedef typename super::reference			reference;
+					typedef typename super::const_reference		const_reference;
+					typedef typename super::pointer				pointer;
+					typedef typename super::const_pointer		const_pointer;
+
+#		if __cplusplus >= 201103L
+					typedef typename super::rvalue_reference			rvalue_reference;
+					typedef typename super::const_rvalue_reference		const_rvalue_reference;
+#		endif
+
+					typedef typename super::size_type					size_type;
+					typedef typename super::difference_type				difference_type;
+
+					typedef typename super::iterator					iterator;
+					typedef typename super::const_iterator				const_iterator;
+					typedef typename super::reverse_iterator			reverse_iterator;
+					typedef typename super::const_reverse_iterator		const_reverse_iterator;
+					typedef typename super::unique_insert_r				unique_insert_r;
+
+				protected:
+					flat_map_base() :
+							super()
+					{
+					}
+
+					explicit flat_map_base(key_compare kc) :
+							super(kc)
+					{
+					}
+
+					template <typename InputIterator>
+					flat_map_base(
+							InputIterator first, InputIterator last,
+							typename kerbal::type_traits::enable_if<
+								kerbal::iterator::is_input_compatible_iterator<InputIterator>::value,
+								int
+							>::type = 0
+					) :
+							super()
+					{
+						this->assign(first, last);
+					}
+
+					template <typename InputIterator>
+					flat_map_base(
+							InputIterator first, InputIterator last, key_compare kc,
+							typename kerbal::type_traits::enable_if<
+								kerbal::iterator::is_input_compatible_iterator<InputIterator>::value,
+								int
+							>::type = 0
+					) :
+							super(kc)
+					{
+						this->assign(first, last);
+					}
+
+#			if __cplusplus >= 201103L
+
+					flat_map_base(std::initializer_list<value_type> src) :
+							flat_map_base(src.begin(), src.end())
+					{
+					}
+
+					flat_map_base(std::initializer_list<value_type> src, key_compare kc) :
+							flat_map_base(src.begin(), src.end(), kc)
+					{
+					}
+
+#			endif
+
+				public:
+
+					size_type count(const key_type & key) const
+					{
+						return this->contains(key) ? 1 : 0;
+					}
+
+					size_type count(const key_type & key, const_iterator hint) const
+					{
+						return this->contains(key, hint) ? 1 : 0;
+					}
+
+					template <typename InputIterator>
+					typename kerbal::type_traits::enable_if<
+							kerbal::iterator::is_input_compatible_iterator<InputIterator>::value
+					>::type
+					assign(InputIterator first, InputIterator last)
+					{
+						this->clear();
+						this->insert(first, last);
+					}
+
+					template <typename InputIterator>
+					typename kerbal::type_traits::enable_if<
+							kerbal::iterator::is_input_compatible_iterator<InputIterator>::value
+					>::type
+					assign(InputIterator first, InputIterator last, key_compare kc)
+					{
+						this->clear();
+						this->key_comp_obj() = kc;
+						this->insert(first, last);
+					}
+
+#			if __cplusplus >= 201103L
+
+					void assign(std::initializer_list<value_type> src)
+					{
+						this->assign(src.begin(), src.end());
+					}
+
+					void assign(std::initializer_list<value_type> src, key_compare kc)
+					{
+						this->assign(src.begin(), src.end(), kc);
+					}
+
+#			endif
+
+					unique_insert_r insert(const_reference src)
+					{
+						return this->ordered.unique_insert(src);
+					}
+
+					unique_insert_r insert(const_iterator hint, const_reference src)
+					{
+						return this->ordered.unique_insert(hint, src);
+					}
+
+#			if __cplusplus >= 201103L
+
+					unique_insert_r insert(rvalue_reference src)
+					{
+						return this->ordered.unique_insert(kerbal::compatibility::move(src));
+					}
+
+					unique_insert_r insert(const_iterator hint, rvalue_reference src)
+					{
+						return this->ordered.unique_insert(hint, kerbal::compatibility::move(src));
+					}
+
+#			endif
+
+					template <typename InputIterator>
+					void insert(InputIterator first, InputIterator last)
+					{
+						this->ordered.unique_insert(first, last);
+					}
+
+					const_iterator erase(const key_type & key)
+					{
+						return this->ordered.erase_one(key);
+					}
+
+					const_iterator erase(const_iterator hint, const key_type & key)
+					{
+						return this->erase(this->find(key, hint));
+					}
+
+					mapped_type& operator[](const key_type& key)
+					{
+						const_iterator pos(this->find(key));
+						if (pos == this->cend()) {
+							return this->insert(std::make_pair(key, mapped_type())).first->second; // TODO
+						} else {
+							return pos->second;
+						}
+					}
+
+					mapped_type& at(const key_type& key)
+					{
+						iterator pos(this->find(key));
+						if (key == this->end()) {
+							kerbal::utility::throw_this_exception_helper<std::out_of_range>::throw_this_exception("not found");
+						}
+						return pos->second;
+					}
+
+					const mapped_type& at(const key_type& key) const
+					{
+						const_iterator pos(this->find(key));
+						if (key == this->cend()) {
+							kerbal::utility::throw_this_exception_helper<std::out_of_range>::throw_this_exception("not found");
+						}
+						return pos->second;
+					}
+
+			};
+
+			template <typename Ordered>
+			class flat_multimap_base : public flat_map_common_base<Ordered>
+			{
+				private:
+					typedef flat_map_common_base<Ordered> super;
+
+				public:
+					typedef typename super::key_compare			key_compare;
+					typedef typename super::key_type			key_type;
+					typedef typename super::mapped_type			mapped_type;
+					typedef typename super::value_type			value_type;
+					typedef typename super::const_type			const_type;
+					typedef typename super::reference			reference;
+					typedef typename super::const_reference		const_reference;
+					typedef typename super::pointer				pointer;
+					typedef typename super::const_pointer		const_pointer;
+
+#		if __cplusplus >= 201103L
+					typedef typename super::rvalue_reference			rvalue_reference;
+					typedef typename super::const_rvalue_reference		const_rvalue_reference;
+#		endif
+
+					typedef typename super::size_type					size_type;
+					typedef typename super::difference_type				difference_type;
+
+					typedef typename super::iterator					iterator;
+					typedef typename super::const_iterator				const_iterator;
+					typedef typename super::reverse_iterator			reverse_iterator;
+					typedef typename super::const_reverse_iterator		const_reverse_iterator;
+
+				protected:
+					flat_multimap_base() :
+							super()
+					{
+					}
+
+					explicit flat_multimap_base(key_compare kc) :
+							super(kc)
+					{
+					}
+
+					template <typename InputIterator>
+					flat_multimap_base(
+							InputIterator first, InputIterator last,
+							typename kerbal::type_traits::enable_if<
+								kerbal::iterator::is_input_compatible_iterator<InputIterator>::value,
+								int
+							>::type = 0
+					) :
+							super()
+					{
+						this->assign(first, last);
+					}
+
+					template <typename InputIterator>
+					flat_multimap_base(
+							InputIterator first, InputIterator last, key_compare kc,
+							typename kerbal::type_traits::enable_if<
+								kerbal::iterator::is_input_compatible_iterator<InputIterator>::value,
+								int
+							>::type = 0
+					) :
+							super(kc)
+					{
+						this->assign(first, last);
+					}
+
+#			if __cplusplus >= 201103L
+
+					flat_multimap_base(std::initializer_list<value_type> src) :
+							flat_multimap_base(src.begin(), src.end())
+					{
+					}
+
+					flat_multimap_base(std::initializer_list<value_type> src, key_compare kc) :
+							flat_multimap_base(src.begin(), src.end(), kc)
+					{
+					}
+
+#			endif
+
+				public:
+
+					size_type count(const key_type & key) const
+					{
+						return this->ordered.count(key);
+					}
+
+					size_type count(const key_type & key, const_iterator hint) const
+					{
+						return this->ordered.count(key, hint);
+					}
+
+					template <typename InputIterator>
+					typename kerbal::type_traits::enable_if<
+							kerbal::iterator::is_input_compatible_iterator<InputIterator>::value
+					>::type
+					assign(InputIterator first, InputIterator last)
+					{
+						this->ordered.assign(first, last);
+					}
+
+					template <typename InputIterator>
+					typename kerbal::type_traits::enable_if<
+							kerbal::iterator::is_input_compatible_iterator<InputIterator>::value
+					>::type
+					assign(InputIterator first, InputIterator last, key_compare kc)
+					{
+						this->ordered.assign(first, last, kc);
+					}
+
+#			if __cplusplus >= 201103L
+
+					void assign(std::initializer_list<value_type> src)
+					{
+						this->assign(src.begin(), src.end());
+					}
+
+					void assign(std::initializer_list<value_type> src, key_compare kc)
+					{
+						this->assign(src.begin(), src.end(), kc);
+					}
+
+#			endif
+
+					const_iterator insert(const_reference src)
+					{
+						return this->ordered.insert(src);
+					}
+
+					const_iterator insert(const_iterator hint, const_reference src)
+					{
+						return this->ordered.insert(hint, src);
+					}
+
+#			if __cplusplus >= 201103L
+
+					const_iterator insert(rvalue_reference src)
+					{
+						return this->ordered.insert(kerbal::compatibility::move(src));
+					}
+
+					const_iterator insert(const_iterator hint, rvalue_reference src)
+					{
+						return this->ordered.insert(hint, kerbal::compatibility::move(src));
+					}
+
+#			endif
+
+					template <typename InputIterator>
+					void insert(InputIterator first, InputIterator last)
+					{
+						this->ordered.insert(first, last);
+					}
+
+					size_type erase(const key_type & key)
+					{
+						return this->ordered.erase(key);
+					}
+
+			};
+
+		} // namespace detail
+
+	} // namespace container
+
+} // namespace kerbal
+
+
+#endif // KERBAL_CONTAINER_DETAIL_FLAT_MAP_BASE_HPP
---
+++ include/kerbal/container/static_flat_map/static_flat_map.decl.hpp
@@ -0,0 +1,315 @@
+/**
+ * @file       static_flat_map.decl.hpp
+ * @brief
+ * @date       2019-8-24
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_STATIC_FLAT_MAP_STATIC_FLAT_MAP_DECL_HPP
+#define KERBAL_CONTAINER_STATIC_FLAT_MAP_STATIC_FLAT_MAP_DECL_HPP
+
+#include <kerbal/container/static_flat_map/static_flat_map.fwd.hpp>
+
+#include <kerbal/assign/assign_list.hpp>
+#include <kerbal/compatibility/constexpr.hpp>
+#include <kerbal/container/static_ordered.hpp>
+#include <kerbal/container/associative_container_facility/key_extractors/tuple_first_extractor.hpp>
+#include <kerbal/container/associative_container_facility/map_data.hpp>
+
+#include <cstddef>
+
+#if __cplusplus >= 201103L
+#	include <initializer_list>
+#endif
+
+#include <kerbal/container/detail/flat_map_base.hpp>
+
+
+namespace kerbal
+{
+
+	namespace container
+	{
+
+		template <typename Key, typename Value, std::size_t N, typename KeyCompare>
+		class static_flat_map :
+				public kerbal::container::detail::flat_map_base<
+					kerbal::container::static_ordered<
+						kerbal::container::map_data<Key, Value>, N,
+						kerbal::container::tuple_first_extractor<kerbal::container::map_data<Key, Value>, Key>,
+						KeyCompare
+					>
+				>
+		{
+			private:
+				typedef kerbal::container::static_ordered<
+					kerbal::container::map_data<Key, Value>, N,
+					kerbal::container::tuple_first_extractor<kerbal::container::map_data<Key, Value>, Key>,
+					KeyCompare
+				> Ordered;
+				typedef kerbal::container::detail::flat_map_base<Ordered> super;
+
+			public:
+				typedef typename super::key_compare			key_compare;
+				typedef typename super::key_type			key_type;
+				typedef typename super::mapped_type			mapped_type;
+				typedef typename super::value_type			value_type;
+				typedef typename super::const_type			const_type;
+				typedef typename super::reference			reference;
+				typedef typename super::const_reference		const_reference;
+				typedef typename super::pointer				pointer;
+				typedef typename super::const_pointer		const_pointer;
+
+#		if __cplusplus >= 201103L
+				typedef typename super::rvalue_reference			rvalue_reference;
+				typedef typename super::const_rvalue_reference		const_rvalue_reference;
+#		endif
+
+				typedef typename super::size_type					size_type;
+				typedef typename super::difference_type				difference_type;
+
+				typedef typename super::iterator					iterator;
+				typedef typename super::const_iterator				const_iterator;
+				typedef typename super::reverse_iterator			reverse_iterator;
+				typedef typename super::const_reverse_iterator		const_reverse_iterator;
+
+			public:
+
+				KERBAL_CONSTEXPR
+				static_flat_map() :
+						super()
+				{
+				}
+
+				KERBAL_CONSTEXPR
+				explicit static_flat_map(key_compare kc) :
+						super(kc)
+				{
+				}
+
+				template <typename InputIterator>
+				KERBAL_CONSTEXPR14
+				static_flat_map(InputIterator first, InputIterator last,
+						typename kerbal::type_traits::enable_if<
+							kerbal::iterator::is_input_compatible_iterator<InputIterator>::value,
+							int
+						>::type = 0
+				) :
+						super(first, last)
+				{
+				}
+
+				template <typename InputIterator>
+				KERBAL_CONSTEXPR14
+				static_flat_map(InputIterator first, InputIterator last, key_compare kc,
+						typename kerbal::type_traits::enable_if<
+							kerbal::iterator::is_input_compatible_iterator<InputIterator>::value,
+							int
+						>::type = 0
+				) :
+						super(first, last, kc)
+				{
+				}
+
+#		if __cplusplus >= 201103L
+
+				KERBAL_CONSTEXPR14
+				static_flat_map(std::initializer_list<value_type> ilist) :
+						super(ilist)
+				{
+				}
+
+				KERBAL_CONSTEXPR14
+				static_flat_map(std::initializer_list<value_type> ilist, key_compare kc) :
+						super(ilist, kc)
+				{
+				}
+
+#		else
+
+				template <typename U>
+				static_flat_map(const kerbal::assign::assign_list<U> & ilist) :
+						super(ilist)
+				{
+				}
+
+				template <typename U>
+				static_flat_map(const kerbal::assign::assign_list<U> & ilist, key_compare kc) :
+						super(ilist, kc)
+				{
+				}
+
+#		endif
+
+				KERBAL_CONSTEXPR14
+				void assign(const static_flat_map & src)
+				{
+					this->ordered.assign(src.ordered);
+				}
+
+				KERBAL_CONSTEXPR14
+				static_flat_map& operator=(const static_flat_map & src)
+				{
+					this->assign(src);
+					return *this;
+				}
+
+#		if __cplusplus >= 201103L
+
+				KERBAL_CONSTEXPR14
+				static_flat_map& operator=(std::initializer_list<value_type> ilist)
+				{
+					this->assign(ilist);
+					return *this;
+				}
+
+				KERBAL_CONSTEXPR14
+				void assign(std::initializer_list<value_type> ilist)
+				{
+					this->super::assign(ilist);
+				}
+
+#		else
+
+				template <typename U>
+				static_flat_map& operator=(const kerbal::assign::assign_list<U> & ilist)
+				{
+					this->assign(ilist);
+					return *this;
+				}
+
+				template <typename U>
+				void assign(const kerbal::assign::assign_list<U> & ilist)
+				{
+					this->super::assign(ilist);
+				}
+
+#		endif
+
+				KERBAL_CONSTEXPR
+				bool full() const
+				{
+					return this->ordered.full();
+				}
+
+				KERBAL_CONSTEXPR14
+				void swap(static_flat_map & ano)
+				{
+					this->ordered.swap(ano.ordered);
+				}
+
+
+				template <typename Key2, typename Value2, typename KeyCompare2, std::size_t N1, std::size_t N2>
+				KERBAL_CONSTEXPR14
+				friend bool operator==(
+						const static_flat_map<Key2, Value2, N1, KeyCompare2> & lhs,
+						const static_flat_map<Key2, Value2, N2, KeyCompare2> & rhs
+				);
+
+				template <typename Key2, typename Value2, typename KeyCompare2, std::size_t N1, std::size_t N2>
+				KERBAL_CONSTEXPR14
+				friend bool operator!=(
+						const static_flat_map<Key2, Value2, N1, KeyCompare2> & lhs,
+						const static_flat_map<Key2, Value2, N2, KeyCompare2> & rhs
+				);
+
+				template <typename Key2, typename Value2, typename KeyCompare2, std::size_t N1, std::size_t N2>
+				KERBAL_CONSTEXPR14
+				friend bool operator<(
+						const static_flat_map<Key2, Value2, N1, KeyCompare2> & lhs,
+						const static_flat_map<Key2, Value2, N2, KeyCompare2> & rhs
+				);
+
+				template <typename Key2, typename Value2, typename KeyCompare2, std::size_t N1, std::size_t N2>
+				KERBAL_CONSTEXPR14
+				friend bool operator<=(
+						const static_flat_map<Key2, Value2, N1, KeyCompare2> & lhs,
+						const static_flat_map<Key2, Value2, N2, KeyCompare2> & rhs
+				);
+
+				template <typename Key2, typename Value2, typename KeyCompare2, std::size_t N1, std::size_t N2>
+				KERBAL_CONSTEXPR14
+				friend bool operator>(
+						const static_flat_map<Key2, Value2, N1, KeyCompare2> & lhs,
+						const static_flat_map<Key2, Value2, N2, KeyCompare2> & rhs
+				);
+
+				template <typename Key2, typename Value2, typename KeyCompare2, std::size_t N1, std::size_t N2>
+				KERBAL_CONSTEXPR14
+				friend bool operator>=(
+						const static_flat_map<Key2, Value2, N1, KeyCompare2> & lhs,
+						const static_flat_map<Key2, Value2, N2, KeyCompare2> & rhs
+				);
+
+		};
+
+		template <typename Key, typename Value, typename KeyCompare, std::size_t M, std::size_t N>
+		KERBAL_CONSTEXPR14
+		bool operator==(
+				const static_flat_map<Key, Value, M, KeyCompare> & lhs,
+				const static_flat_map<Key, Value, N, KeyCompare> & rhs
+		)
+		{
+			return lhs.ordered == rhs.ordered;
+		}
+
+		template <typename Key, typename Value, typename KeyCompare, std::size_t M, std::size_t N>
+		KERBAL_CONSTEXPR14
+		bool operator!=(
+				const static_flat_map<Key, Value, M, KeyCompare> & lhs,
+				const static_flat_map<Key, Value, N, KeyCompare> & rhs
+		)
+		{
+			return lhs.ordered != rhs.ordered;
+		}
+
+		template <typename Key, typename Value, typename KeyCompare, std::size_t M, std::size_t N>
+		KERBAL_CONSTEXPR14
+		bool operator<(
+				const static_flat_map<Key, Value, M, KeyCompare> & lhs,
+				const static_flat_map<Key, Value, N, KeyCompare> & rhs
+		)
+		{
+			return lhs.ordered < rhs.ordered;
+		}
+
+		template <typename Key, typename Value, typename KeyCompare, std::size_t M, std::size_t N>
+		KERBAL_CONSTEXPR14
+		bool operator<=(
+				const static_flat_map<Key, Value, M, KeyCompare> & lhs,
+				const static_flat_map<Key, Value, N, KeyCompare> & rhs
+		)
+		{
+			return lhs.ordered <= rhs.ordered;
+		}
+
+		template <typename Key, typename Value, typename KeyCompare, std::size_t M, std::size_t N>
+		KERBAL_CONSTEXPR14
+		bool operator>(
+				const static_flat_map<Key, Value, M, KeyCompare> & lhs,
+				const static_flat_map<Key, Value, N, KeyCompare> & rhs
+		)
+		{
+			return lhs.ordered > rhs.ordered;
+		}
+
+		template <typename Key, typename Value, typename KeyCompare, std::size_t M, std::size_t N>
+		KERBAL_CONSTEXPR14
+		bool operator>=(
+				const static_flat_map<Key, Value, M, KeyCompare> & lhs,
+				const static_flat_map<Key, Value, N, KeyCompare> & rhs
+		)
+		{
+			return lhs.ordered >= rhs.ordered;
+		}
+
+	} // namespace container
+
+} // namespace kerbal
+
+
+#endif // KERBAL_CONTAINER_STATIC_FLAT_MAP_STATIC_FLAT_MAP_DECL_HPP
