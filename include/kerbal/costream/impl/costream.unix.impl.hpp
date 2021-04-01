c8b080f89ec140a0b23fcc77f538b716a4c45b8a
WentsingNee <1205422966@qq.com>
2021-04-01 19:30:10 +0800

    costream 独立成库

---
+++ include/kerbal/costream/impl/costream.unix.impl.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file       costream.unix.impl.hpp
+ * @brief
+ * @date       2020-04-15
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_COSTREAM_COSTREAM_UNIX_IMPL_HPP
+#define KERBAL_COSTREAM_COSTREAM_UNIX_IMPL_HPP
+
+#endif // KERBAL_COSTREAM_COSTREAM_UNIX_IMPL_HPP
