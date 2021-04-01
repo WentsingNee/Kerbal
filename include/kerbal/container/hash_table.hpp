9ae0c0e3dea1e6a042655680bf421a39284bd71e
WentsingNee <1205422966@qq.com>
2021-04-01 22:26:45 +0800

    新增 hash_table 2

---
+++ include/kerbal/container/hash_table.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file       hash_table.hpp
+ * @brief
+ * @date       2020-08-29
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_HASH_TABLE_HPP
+#define KERBAL_CONTAINER_HASH_TABLE_HPP
+
+#include <kerbal/container/hash_table/hash_table.fwd.hpp>
+#include <kerbal/container/hash_table/hash_table.decl.hpp>
+#include <kerbal/container/hash_table/hash_table.impl.hpp>
+
+#endif // KERBAL_CONTAINER_HASH_TABLE_HPP
