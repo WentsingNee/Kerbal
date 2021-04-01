18851775a21d179c9622d9930d3ebb1093da1b6d
WentsingNee <1205422966@qq.com>
2021-04-01 19:27:32 +0800

    新增 coroutine 子库

---
+++ include/kerbal/coroutine/lazy/lazy.fwd.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file       lazy.fwd.hpp
+ * @brief
+ * @date       2021-08-16
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_COROUTINE_LAZY_LAZY_FWD_HPP
+#define KERBAL_COROUTINE_LAZY_LAZY_FWD_HPP
+
+
+namespace kerbal
+{
+
+	namespace coroutine
+	{
+
+		template <typename T = void>
+		class lazy;
+
+		class bad_lazy;
+
+	} // namespace coroutine
+
+} // namespace kerbal
+
+#endif // KERBAL_COROUTINE_LAZY_LAZY_FWD_HPP
