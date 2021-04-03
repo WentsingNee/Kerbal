c27d19f5e1f6649f2347c8a42a713706baad7602
WentsingNee <1205422966@qq.com>
2021-04-04 01:40:51 +0800

    algorithm 子库新增 inplace_merge 算法

--- include/kerbal/algorithm/modifier.hpp
+++ include/kerbal/algorithm/modifier.hpp
@@ -12,6 +12,7 @@
 #ifndef KERBAL_ALGORITHM_MODIFIER_HPP
 #define KERBAL_ALGORITHM_MODIFIER_HPP
 
+#include <kerbal/algorithm/binary_search.hpp>
 #include <kerbal/algorithm/binary_type_predicate.hpp>
 #include <kerbal/algorithm/querier.hpp>
 #include <kerbal/algorithm/swap.hpp>
@@ -731,6 +732,113 @@ namespace kerbal
 		{
 			to = kerbal::algorithm::copy(mid, last, to);
 			return kerbal::algorithm::copy(first, mid, to);
+		}
+
+
+		namespace detail
+		{
+
+			template <typename ForwardIterator, typename BinaryPredict>
+			KERBAL_CONSTEXPR14
+			void _K_inplace_merge_fwd_iter_loop(ForwardIterator first, ForwardIterator mid, ForwardIterator last, BinaryPredict cmp)
+			{
+				typedef ForwardIterator iterator;
+
+				while (static_cast<bool>(first != mid) && static_cast<bool>(mid != last)) {
+
+					if (kerbal::iterator::next(first) == mid) {
+						while (mid != last) {
+							if (cmp(*mid, *first)) { // *first > *mid
+								kerbal::algorithm::iter_swap(first, mid);
+								++first;
+								++mid;
+							} else {
+								break;
+							}
+						}
+						return;
+					}
+
+					iterator lmid(kerbal::iterator::midden_iterator(first, mid));
+					iterator rmid(kerbal::algorithm::lower_bound(mid, last, *lmid, cmp));
+					mid = kerbal::algorithm::rotate(lmid, mid, rmid);
+					_K_inplace_merge_fwd_iter_loop(first, lmid, mid, cmp);
+
+					first = mid;
+					mid = rmid; // kerbal::algorithm::inplace_merge(mid, rmid, last, cmp);
+				}
+			}
+
+			template <typename ForwardIterator, typename BinaryPredict>
+			KERBAL_CONSTEXPR14
+			void _K_inplace_merge(ForwardIterator first, ForwardIterator mid, ForwardIterator last, BinaryPredict cmp, std::forward_iterator_tag)
+			{
+				_K_inplace_merge_fwd_iter_loop(first, mid, last, cmp);
+			}
+
+			template <typename RandomAccessIterator, typename BinaryPredict>
+			KERBAL_CONSTEXPR14
+			void _K_inplace_merge_ras_iter_loop(RandomAccessIterator first, RandomAccessIterator mid, RandomAccessIterator last, BinaryPredict cmp)
+			{
+				typedef RandomAccessIterator iterator;
+				typedef typename kerbal::iterator::iterator_traits<iterator>::difference_type difference_type;
+
+				while (true) {
+					difference_type left_len(kerbal::iterator::distance(first, mid));
+					difference_type right_len(kerbal::iterator::distance(mid, last));
+
+					if (left_len == 0 || right_len == 0) {
+						return;
+					}
+
+					if (left_len == 1 && right_len == 1) {
+						if (cmp(*mid, *first)) { // *first > *mid
+							kerbal::algorithm::iter_swap(first, mid);
+						}
+						return;
+					}
+
+					iterator lmid(first);
+					iterator rmid(mid);
+
+					if (left_len >= right_len) {
+						lmid = kerbal::iterator::midden_iterator(first, mid);
+						rmid = kerbal::algorithm::lower_bound(mid, last, *lmid, cmp);
+					} else {
+						rmid = kerbal::iterator::midden_iterator(mid, last);
+						lmid = kerbal::algorithm::upper_bound(first, mid, *rmid, cmp);
+					}
+					mid = kerbal::algorithm::rotate(lmid, mid, rmid);
+					_K_inplace_merge_ras_iter_loop(first, lmid, mid, cmp);
+
+					first = mid;
+					mid = rmid; // kerbal::algorithm::inplace_merge(mid, rmid, last, cmp);
+				}
+			}
+
+			template <typename RandomAccessIterator, typename BinaryPredict>
+			KERBAL_CONSTEXPR14
+			void _K_inplace_merge(RandomAccessIterator first, RandomAccessIterator mid, RandomAccessIterator last, BinaryPredict cmp, std::random_access_iterator_tag)
+			{
+				_K_inplace_merge_ras_iter_loop(first, mid, last, cmp);
+			}
+
+		} // namespace detail
+
+		template <typename ForwardIterator, typename BinaryPredict>
+		KERBAL_CONSTEXPR14
+		void inplace_merge(ForwardIterator first, ForwardIterator mid, ForwardIterator last, BinaryPredict cmp)
+		{
+			detail::_K_inplace_merge(first, mid, last, cmp, kerbal::iterator::iterator_category(first));
+		}
+
+		template <typename ForwardIterator>
+		KERBAL_CONSTEXPR14
+		void inplace_merge(ForwardIterator first, ForwardIterator mid, ForwardIterator last)
+		{
+			typedef ForwardIterator iterator;
+			typedef typename kerbal::iterator::iterator_traits<iterator>::value_type value_type;
+			kerbal::algorithm::inplace_merge(first, mid, last, std::less<value_type>());
 		}
 
 
