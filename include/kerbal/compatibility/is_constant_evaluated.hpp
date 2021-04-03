55be6fe6453dbba00eb04ff6630c687e12c0e485
WentsingNee <1205422966@qq.com>
2021-04-03 19:46:32 +0800

    新增 compatibility/is_constant_evaluated

---
+++ include/kerbal/compatibility/is_constant_evaluated.hpp
@@ -0,0 +1,73 @@
+/**
+ * @file       is_constant_evaluated.hpp
+ * @brief
+ * @date       2021-1-1
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_COMPATIBILITY_IS_CONSTANT_EVALUATED_HPP
+#define KERBAL_COMPATIBILITY_IS_CONSTANT_EVALUATED_HPP
+
+#include <kerbal/config/compiler_id.hpp>
+#include <kerbal/config/compiler_private.hpp>
+
+
+#ifndef KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT
+
+#	if KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_GNU
+
+#		if KERBAL_GNU_PRIVATE_HAS_BUILTIN(__builtin_is_constant_evaluated)
+#			define KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT 1
+#			define KERBAL_IS_CONSTANT_EVALUATED() (__builtin_is_constant_evaluated())
+#		endif
+
+#	elif KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_CLANG
+
+#		if KERBAL_CLANG_PRIVATE_HAS_BUILTIN(__builtin_is_constant_evaluated)
+#			define KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT 1
+#			define KERBAL_IS_CONSTANT_EVALUATED() (__builtin_is_constant_evaluated())
+#		endif
+
+#	elif KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_ICC
+
+#		if KERBAL_ICC_PRIVATE_HAS_BUILTIN(__builtin_is_constant_evaluated)
+#			define KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT 1
+#			define KERBAL_IS_CONSTANT_EVALUATED() (__builtin_is_constant_evaluated())
+#		endif
+
+#	endif
+
+
+#	ifndef KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT
+#		if __cplusplus > 201703L
+#			include <type_traits>
+#			if __cpp_lib_is_constant_evaluated >= 201811L
+#				define KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT 1
+#				define KERBAL_IS_CONSTANT_EVALUATED() (std::is_constant_evaluated())
+#			endif
+#		endif
+#	endif
+
+
+#	ifndef KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT
+#		define KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT 0
+#	endif
+
+#else // ifdef KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT
+
+#	if KERBAL_HAS_IS_CONSTANT_EVALUATED_SUPPORT
+
+#		ifndef KERBAL_IS_CONSTANT_EVALUATED
+#			error "Macro KERBAL_IS_CONSTANT_EVALUATED has not been defined yet!"
+#		endif
+
+#	endif
+
+#endif
+
+
+#endif // KERBAL_COMPATIBILITY_IS_CONSTANT_EVALUATED_HPP
