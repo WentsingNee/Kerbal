f5ef596657d53b97f16408a2e4cec6fb4498b53e
WentsingNee <1205422966@qq.com>
2021-04-06 16:57:55 +0800

    修改 config 子库

---
+++ include/kerbal/config/target_type.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file       target_type.hpp
+ * @brief
+ * @date       2020-09-23
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONFIG_TARGET_TYPE_HPP
+#define KERBAL_CONFIG_TARGET_TYPE_HPP
+
+#ifndef KERBAL_TARGET_TYPE_HEADER
+#	define KERBAL_TARGET_TYPE_HEADER 0
+#endif
+
+#ifndef KERBAL_TARGET_TYPE_STATIC
+#	define KERBAL_TARGET_TYPE_STATIC 1
+#endif
+
+#ifndef KERBAL_TARGET_TYPE_SHARED
+#	define KERBAL_TARGET_TYPE_SHARED 2
+#endif
+
+
+#ifndef KERBAL_TARGET_TYPE
+#	define KERBAL_TARGET_TYPE KERBAL_TARGET_TYPE_HEADER
+#endif
+
+
+#endif // KERBAL_CONFIG_TARGET_TYPE_HPP
