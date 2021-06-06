1b35d984c82f23f7f5b72bb3abcb534b0937f1c1
WentsingNee <1205422966@qq.com>
2021-06-06 17:35:44 +0800

    使用编译器内建方法优化 make_integer_sequence

--- include/kerbal/utility/integer_sequence.hpp
+++ include/kerbal/utility/integer_sequence.hpp
@@ -17,9 +17,12 @@
 for the ISO C++ 2011 standard.
 #endif
 
+#include <kerbal/config/compiler_private.hpp>
+
 #include <type_traits>
 
 #include <cstddef>
+
 
 namespace kerbal
 {
@@ -33,11 +36,57 @@ namespace kerbal
 				typedef T value_type;
 
 				constexpr
-				static size_t size() noexcept
+				static std::size_t size() noexcept
 				{
 					return sizeof...(Ints);
 				}
 		};
+
+		template <std::size_t ... Ints>
+		using index_sequence = kerbal::utility::integer_sequence<std::size_t, Ints...>;
+
+
+
+#define KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_DEFAULT					0
+#define KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_GNU_INTEGER_PACK			1
+#define KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_CLANG_MAKE_INTEGER_SEQ		2
+
+
+#if KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_GNU
+
+//	https://gcc.gnu.org/onlinedocs/gcc-8.1.0/gcc/Type-Traits.html#Type-Traits
+#	if KERBAL_GNU_PRIVATE_HAS_BUILTIN(__integer_pack)
+#		define KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY			KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_GNU_INTEGER_PACK
+#	endif
+
+#elif KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_CLANG
+
+#	if KERBAL_CLANG_PRIVATE_HAS_BUILTIN(__make_integer_seq)
+#		define KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY			KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_CLANG_MAKE_INTEGER_SEQ
+#	endif
+
+#elif KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_MSVC
+
+//	https://www.cnblogs.com/yamboo/p/14030418.html
+#	if KERBAL_MSVC_VERSION_MEETS(19, 0, 0) // MSVC++ 14.0  _MSC_VER == 1900 (Visual Studio 2015 version 14.0)
+#		define KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY			KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_CLANG_MAKE_INTEGER_SEQ
+#	endif
+
+
+#elif KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_ICC
+
+#	if KERBAL_ICC_VERSION_MEETS(2021, 2, 0)
+#		define KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY			KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_GNU_INTEGER_PACK
+#	endif
+
+#else
+
+#		define KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY			KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_DEFAULT
+
+#endif
+
+
+#	if KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY == KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_DEFAULT
 
 		namespace detail
 		{
@@ -79,15 +128,22 @@ namespace kerbal
 
 		} // namespace detail
 
-		template <size_t ... Ints>
-		using index_sequence = kerbal::utility::integer_sequence<size_t, Ints...>;
+#	endif
+
 
 		template <typename T, T N>
-		using make_integer_sequence = typename kerbal::utility::detail::make_integer_sequence_helper<T, N>::type;
+		using make_integer_sequence =
+#	if   KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY == KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_DEFAULT
+				typename kerbal::utility::detail::make_integer_sequence_helper<T, N>::type;
+#	elif KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY == KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_GNU_INTEGER_PACK
+				integer_sequence<T, __integer_pack(N)...>;
+#	elif KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY == KERBAL_UTILITY_MAKE_INTEGER_SEQUENCE_IMPL_POLICY_CLANG_MAKE_INTEGER_SEQ
+				__make_integer_seq<integer_sequence, T, N>;
+#	endif
 		// kerbal::utility::integer_sequence<T, /* a sequence 0, 1, 2, ..., N-1 */ >;
 
-		template <size_t N>
-		using make_index_sequence = kerbal::utility::make_integer_sequence<size_t, N>;
+		template <std::size_t N>
+		using make_index_sequence = kerbal::utility::make_integer_sequence<std::size_t, N>;
 
 	} // namespace utility
 
