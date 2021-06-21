5bc0b2d1c2ee31ce1f674ad0a9f80c665fabd08b
WentsingNee <1205422966@qq.com>
2021-06-21 12:13:16 +0800

    querier range 化改造

--- include/kerbal/algorithm/querier/all_of.hpp
+++ include/kerbal/algorithm/querier/all_of.hpp
@@ -1,7 +1,7 @@
 /**
  * @file       all_of.hpp
  * @brief
- * @date       2023-08-24
+ * @date       2021-06-19
  * @author     Peter
  * @copyright
  *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
@@ -14,6 +14,9 @@
 
 #include <kerbal/algorithm/querier/find_if_not.hpp>
 #include <kerbal/compatibility/constexpr.hpp>
+#include <kerbal/container/nonmember_container_access.hpp>
+
+#include <cstddef>
 
 
 namespace kerbal
@@ -22,11 +25,25 @@ namespace kerbal
 	namespace algorithm
 	{
 
-		template <typename InputIterator, typename UnaryPredicate>
+		template <typename InputIterator, typename SentinelIterator, typename UnaryPredicate>
 		KERBAL_CONSTEXPR14
-		bool all_of(InputIterator first, InputIterator last, UnaryPredicate pred)
+		bool all_of(InputIterator first, SentinelIterator last, UnaryPredicate pred)
 		{
 			return static_cast<bool>(kerbal::algorithm::find_if_not(first, last, pred) == last);
+		}
+
+		template <typename T, std::size_t N, typename UnaryPredicate>
+		KERBAL_CONSTEXPR14
+		bool all_of(const T (&a) [N], UnaryPredicate pred)
+		{
+			return kerbal::algorithm::all_of(kerbal::container::begin(a), kerbal::container::end(a), pred);
+		}
+
+		template <typename InputRange, typename UnaryPredicate>
+		KERBAL_CONSTEXPR14
+		bool all_of(const InputRange & range, UnaryPredicate pred)
+		{
+			return kerbal::algorithm::all_of(kerbal::container::begin(range), kerbal::container::end(range), pred);
 		}
 
 	} // namespace algorithm
--- include/kerbal/algorithm/querier/find_if_not.hpp
+++ include/kerbal/algorithm/querier/find_if_not.hpp
@@ -1,7 +1,7 @@
 /**
  * @file       find_if_not.hpp
  * @brief
- * @date       2023-08-24
+ * @date       2021-06-19
  * @author     Peter
  * @copyright
  *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
@@ -13,6 +13,9 @@
 #define KERBAL_ALGORITHM_QUERIER_FIND_IF_NOT_HPP
 
 #include <kerbal/compatibility/constexpr.hpp>
+#include <kerbal/container/nonmember_container_access.hpp>
+
+#include <cstddef>
 
 
 namespace kerbal
@@ -21,10 +24,10 @@ namespace kerbal
 	namespace algorithm
 	{
 
-		template <typename InputIterator, typename UnaryPredicate>
+		template <typename InputIterator, typename SentinelIterator, typename UnaryPredicate>
 		KERBAL_CONSTEXPR14
 		InputIterator
-		find_if_not(InputIterator first, InputIterator last, UnaryPredicate pred)
+		find_if_not(InputIterator first, SentinelIterator last, UnaryPredicate pred)
 		{
 			while (first != last) {
 				if (pred(*first)) {
@@ -34,6 +37,21 @@ namespace kerbal
 				}
 			}
 			return first;
+		}
+
+		template <typename T, std::size_t N, typename UnaryPredicate>
+		KERBAL_CONSTEXPR14
+		T* find_if_not(T (&a) [N], UnaryPredicate pred)
+		{
+			return kerbal::algorithm::find_if_not(kerbal::container::begin(a), kerbal::container::end(a), pred);
+		}
+
+		template <typename InputRange, typename UnaryPredicate>
+		KERBAL_CONSTEXPR14
+		typename kerbal::container::range_traits<InputRange>::iterator
+		find_if_not(InputRange & range, UnaryPredicate pred)
+		{
+			return kerbal::algorithm::find_if_not(kerbal::container::begin(range), kerbal::container::end(range), pred);
 		}
 
 	} // namespace algorithm
