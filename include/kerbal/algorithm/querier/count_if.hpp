dfa7a5d08e9c09158efc40e3f0ce29b7c1043cbb
WentsingNee <1205422966@qq.com>
2021-06-21 12:13:16 +0800

    querier range 化改造

--- include/kerbal/algorithm/querier/count_if.hpp
+++ include/kerbal/algorithm/querier/count_if.hpp
@@ -1,7 +1,7 @@
 /**
  * @file       count_if.hpp
  * @brief
- * @date       2023-08-24
+ * @date       2021-06-19
  * @author     Peter
  * @copyright
  *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
@@ -13,6 +13,7 @@
 #define KERBAL_ALGORITHM_QUERIER_COUNT_IF_HPP
 
 #include <kerbal/compatibility/constexpr.hpp>
+#include <kerbal/container/nonmember_container_access.hpp>
 
 #include <cstddef>
 
@@ -23,9 +24,9 @@ namespace kerbal
 	namespace algorithm
 	{
 
-		template <typename InputIterator, typename UnaryPredicate>
+		template <typename InputIterator, typename SentinelIterator, typename UnaryPredicate>
 		KERBAL_CONSTEXPR14
-		std::size_t count_if(InputIterator first, InputIterator last, UnaryPredicate pred)
+		std::size_t count_if(InputIterator first, SentinelIterator last, UnaryPredicate pred)
 		{
 			std::size_t cnt = 0;
 			while (first != last) {
@@ -35,6 +36,20 @@ namespace kerbal
 				++first;
 			}
 			return cnt;
+		}
+
+		template <typename T, std::size_t N, typename UnaryPredicate>
+		KERBAL_CONSTEXPR14
+		std::size_t count_if(const T (&a) [N], UnaryPredicate pred)
+		{
+			return kerbal::algorithm::count_if(kerbal::container::begin(a), kerbal::container::end(a), pred);
+		}
+
+		template <typename InputRange, typename UnaryPredicate>
+		KERBAL_CONSTEXPR14
+		std::size_t count_if(const InputRange & range, UnaryPredicate pred)
+		{
+			return kerbal::algorithm::count_if(kerbal::container::begin(range), kerbal::container::end(range), pred);
 		}
 
 	} // namespace algorithm
