60610f42c24a5b6377aaf61dfd00e36cc05516f7
WentsingNee <1205422966@qq.com>
2021-08-09 18:55:42 +0800

    使用 uninitialized 系列组件优化、精简 raw_storage

--- include/kerbal/data_struct/raw_storage.hpp
+++ include/kerbal/data_struct/raw_storage.hpp
@@ -12,17 +12,14 @@
 #ifndef KERBAL_DATA_STRUCT_RAW_STORAGE_HPP
 #define KERBAL_DATA_STRUCT_RAW_STORAGE_HPP
 
-#include <kerbal/algorithm/modifier.hpp>
 #include <kerbal/compatibility/alignof.hpp>
 #include <kerbal/compatibility/constexpr.hpp>
 #include <kerbal/compatibility/method_overload_tag.hpp>
 #include <kerbal/compatibility/move.hpp>
 #include <kerbal/compatibility/noexcept.hpp>
 #include <kerbal/memory/uninitialized.hpp>
-#include <kerbal/operators/generic_assign.hpp>
 #include <kerbal/type_traits/aligned_storage.hpp>
 #include <kerbal/type_traits/array_traits.hpp>
-#include <kerbal/type_traits/enable_if.hpp>
 #include <kerbal/utility/declval.hpp>
 #include <kerbal/utility/noncopyable.hpp>
 
@@ -38,6 +35,7 @@
 #	include <kerbal/type_traits/pointer_deduction.hpp>
 #endif
 
+
 namespace kerbal
 {
 
@@ -52,7 +50,8 @@ namespace kerbal
 		template <typename Type>
 		struct is_trivial_stored_type:
 				kerbal::type_traits::bool_constant<
-						std::is_trivial<Type>::value
+					std::is_trivially_default_constructible<typename kerbal::type_traits::remove_all_extents<Type>::type>::value &&
+					std::is_trivially_destructible<typename kerbal::type_traits::remove_all_extents<Type>::type>::value
 				>
 		{
 		};
@@ -73,12 +72,12 @@ namespace kerbal
 
 
 
-		template <typename ValueType, bool is_trival = is_trivial_stored_type<ValueType>::value>
-		class __rawst_base;
+		template <typename ValueType, bool is_trivally_stored = is_trivial_stored_type<ValueType>::value>
+		class _K_rawst_base;
 
 		template <typename ValueType>
-		class __rawst_base<ValueType, true>
-						: private kerbal::utility::noncopyable
+		class _K_rawst_base<ValueType, true>:
+						private kerbal::utility::noncopyable
 		{
 			protected:
 				typedef ValueType				value_type;
@@ -95,26 +94,28 @@ namespace kerbal
 
 				typedef ValueType				storage_type;
 
-				storage_type storage;
+				storage_type _K_storage;
 
-				KERBAL_CONSTEXPR
-				__rawst_base() KERBAL_NOEXCEPT
-						: storage()
-				{
-				}
+			protected:
+
+#		if __cplusplus < 201103L
+				_K_rawst_base() KERBAL_NOEXCEPT {}
+#		else
+				_K_rawst_base() = default;
+#		endif
 
 			public:
 
 				KERBAL_CONSTEXPR14 // note: mustn't be constexpr11 otherwise the compile can't distinguish this method with the const version
 				reference raw_value() KERBAL_REFERENCE_OVERLOAD_TAG KERBAL_NOEXCEPT
 				{
-					return this->storage;
+					return this->_K_storage;
 				}
 
 				KERBAL_CONSTEXPR14
 				const_reference raw_value() const KERBAL_REFERENCE_OVERLOAD_TAG KERBAL_NOEXCEPT
 				{
-					return this->storage;
+					return this->_K_storage;
 				}
 
 #		if __cplusplus >= 201103L
@@ -122,13 +123,13 @@ namespace kerbal
 				KERBAL_CONSTEXPR14
 				rvalue_reference raw_value() && noexcept
 				{
-					return static_cast<rvalue_reference>(this->storage);
+					return static_cast<rvalue_reference>(this->_K_storage);
 				}
 
 				KERBAL_CONSTEXPR14
 				const_rvalue_reference raw_value() const && noexcept
 				{
-					return static_cast<const_rvalue_reference>(this->storage);
+					return static_cast<const_rvalue_reference>(this->_K_storage);
 				}
 
 #		endif
@@ -136,20 +137,25 @@ namespace kerbal
 				KERBAL_CONSTEXPR14
 				pointer raw_pointer() KERBAL_NOEXCEPT
 				{
-					return &(this->storage);
+					return &(this->_K_storage);
 				}
 
 				KERBAL_CONSTEXPR14
 				const_pointer raw_pointer() const KERBAL_NOEXCEPT
 				{
-					return &(this->storage);
+					return &(this->_K_storage);
+				}
+
+				KERBAL_CONSTEXPR14
+				void destroy() KERBAL_NOEXCEPT
+				{
 				}
 
 		};
 
 		template <typename ValueType>
-		class __rawst_base<ValueType, false>
-						: private kerbal::utility::noncopyable
+		class _K_rawst_base<ValueType, false>:
+						private kerbal::utility::noncopyable
 		{
 			protected:
 				typedef ValueType				value_type;
@@ -168,13 +174,15 @@ namespace kerbal
 					typename kerbal::type_traits::aligned_storage<sizeof(ValueType), KERBAL_ALIGNOF(ValueType)>::type
 				storage_type;
 
-				storage_type storage;
+				storage_type _K_storage;
 
-				KERBAL_CONSTEXPR
-				__rawst_base() KERBAL_NOEXCEPT
-						: storage()
-				{
-				}
+			protected:
+
+#		if __cplusplus < 201103L
+				_K_rawst_base() KERBAL_NOEXCEPT {}
+#		else
+				_K_rawst_base() = default;
+#		endif
 
 			public:
 
@@ -204,164 +212,43 @@ namespace kerbal
 
 				pointer raw_pointer() KERBAL_NOEXCEPT
 				{
-					return reinterpret_cast<pointer>(&(this->storage));
+					return reinterpret_cast<pointer>(&(this->_K_storage));
 				}
 
 				const_pointer raw_pointer() const KERBAL_NOEXCEPT
 				{
-					return reinterpret_cast<const_pointer>(&(this->storage));
+					return reinterpret_cast<const_pointer>(&(this->_K_storage));
+				}
+
+				void destroy() KERBAL_NOEXCEPT
+				{
+					kerbal::memory::destroy_at(this->raw_pointer());
 				}
 
 		};
 
 
 
-		template <typename Type, bool is_trival = is_trivial_stored_type<Type>::value>
-		class __rawst_agent;
+		template <typename Type>
+		class _K_rawst_agent;
 
 		template <typename Type>
-		class __rawst_agent<Type, true>: public kerbal::data_struct::__rawst_base<Type>
+		class _K_rawst_agent:
+				public kerbal::data_struct::_K_rawst_base<Type>
 		{
+			private:
+				typedef _K_rawst_agent this_type;
+
 			protected:
 				typedef Type value_type;
-				typedef const value_type&		const_reference;
 
-#		if __cplusplus >= 201103L
-				typedef value_type&&			rvalue_reference;
-#		endif
-
-				KERBAL_CONSTEXPR
-				__rawst_agent() KERBAL_NOEXCEPT
-				{
-				}
-
-			public:
-
-				KERBAL_CONSTEXPR14
-				void construct(const_reference src) KERBAL_NOEXCEPT
-				{
-					this->raw_value() = src;
-				}
+			protected:
 
 #		if __cplusplus < 201103L
-
-				void construct() KERBAL_NOEXCEPT
-				{
-					this->raw_value() = value_type();
-				}
-
-				template <typename Arg0>
-				void construct(const Arg0 & arg0) KERBAL_NOEXCEPT
-				{
-					this->raw_value() = value_type(arg0);
-				}
-
-				template <typename Arg0, typename Arg1>
-				void construct(const Arg0 & arg0, const Arg1& arg1) KERBAL_NOEXCEPT
-				{
-					this->raw_value() = value_type(arg0, arg1);
-				}
-
-				template <typename Arg0, typename Arg1, typename Arg2>
-				void construct(const Arg0 & arg0, const Arg1& arg1, const Arg2 & arg2) KERBAL_NOEXCEPT
-				{
-					this->raw_value() = value_type(arg0, arg1, arg2);
-				}
-
-				template <typename Arg0, typename Arg1, typename Arg2, typename Arg3>
-				void construct(const Arg0 & arg0, const Arg1& arg1, const Arg2 & arg2, const Arg3& arg3) KERBAL_NOEXCEPT
-				{
-					this->raw_value() = value_type(arg0, arg1, arg2, arg3);
-				}
-
-				template <typename Arg0, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-				void construct(const Arg0 & arg0, const Arg1& arg1, const Arg2 & arg2, const Arg3& arg3, const Arg4 & arg4) KERBAL_NOEXCEPT
-				{
-					this->raw_value() = value_type(arg0, arg1, arg2, arg3, arg4);
-				}
-
+				_K_rawst_agent() KERBAL_NOEXCEPT {}
 #		else
-
-				KERBAL_CONSTEXPR14
-				void construct(rvalue_reference src) noexcept
-				{
-					this->raw_value() = kerbal::compatibility::move(src);
-				}
-
-				template <typename ... Args>
-				KERBAL_CONSTEXPR14
-				void construct(Args&&... args) noexcept
-				{
-					this->raw_value() = value_type(kerbal::utility::forward<Args>(args)...);
-				}
-
+				_K_rawst_agent() = default;
 #		endif
-
-				KERBAL_CONSTEXPR14
-				void destroy() KERBAL_NOEXCEPT
-				{
-				}
-
-		};
-
-		template <typename Type, size_t N>
-		class __rawst_agent<Type[N], true>: public kerbal::data_struct::__rawst_base<Type[N]>
-		{
-			protected:
-				typedef Type value_type [N];
-				typedef const value_type&		const_reference;
-
-#		if __cplusplus >= 201103L
-				typedef value_type&&			rvalue_reference;
-#		endif
-
-				KERBAL_CONSTEXPR
-				__rawst_agent() KERBAL_NOEXCEPT
-				{
-				}
-
-			public:
-
-				KERBAL_CONSTEXPR14
-				void construct() KERBAL_NOEXCEPT
-				{
-				}
-
-				KERBAL_CONSTEXPR14
-				void construct(const_reference src) KERBAL_NOEXCEPT
-				{
-					kerbal::algorithm::copy(src + 0, src + N, this->storage + 0);
-				}
-
-#		if __cplusplus >= 201103L
-
-				KERBAL_CONSTEXPR14
-				void construct(rvalue_reference src) KERBAL_NOEXCEPT
-				{
-					kerbal::algorithm::move(src + 0, src + N, this->storage + 0);
-				}
-
-#		endif
-
-				KERBAL_CONSTEXPR14
-				void destroy() KERBAL_NOEXCEPT
-				{
-				}
-
-		};
-
-
-		template <typename Type>
-		class __rawst_agent<Type, false>:
-				public kerbal::data_struct::__rawst_base<Type>
-		{
-			protected:
-				typedef Type value_type;
-
-				KERBAL_CONSTEXPR
-				__rawst_agent() KERBAL_NOEXCEPT
-				{
-				}
 
 			public:
 
@@ -405,11 +292,12 @@ namespace kerbal
 #		else
 
 				template <typename ... Args>
-				void construct(Args&& ... args)
+				KERBAL_CONSTEXPR14
+				void construct(Args&&... args)
 						KERBAL_CONDITIONAL_NOEXCEPT(
 							noexcept(
 								kerbal::memory::construct_at(
-									kerbal::utility::declthis<__rawst_agent>()->raw_pointer(),
+									kerbal::utility::declthis<this_type>()->raw_pointer(),
 									kerbal::utility::forward<Args>(args)...
 								)
 							)
@@ -420,21 +308,15 @@ namespace kerbal
 
 #		endif
 
-				void destroy()
-						KERBAL_CONDITIONAL_NOEXCEPT(
-								std::is_nothrow_destructible<value_type>::value
-						)
-				{
-					kerbal::memory::destroy_at(this->raw_pointer());
-				}
 		};
 
+
 		template <typename Type, size_t N>
-		class __rawst_agent<Type[N], false>:
-				public kerbal::data_struct::__rawst_base<Type[N]>
+		class _K_rawst_agent<Type[N]>:
+				public kerbal::data_struct::_K_rawst_base<Type[N]>
 		{
 			private:
-				typedef __rawst_agent this_type;
+				typedef _K_rawst_agent this_type;
 
 			protected:
 				typedef Type value_type [N];
@@ -446,64 +328,75 @@ namespace kerbal
 
 			protected:
 
-				KERBAL_CONSTEXPR
-				__rawst_agent() KERBAL_NOEXCEPT
-				{
-				}
+#		if __cplusplus < 201103L
+				_K_rawst_agent() KERBAL_NOEXCEPT {}
+#		else
+				_K_rawst_agent() = default;
+#		endif
 
 			public:
 
+				KERBAL_CONSTEXPR14
 				void construct()
 						KERBAL_CONDITIONAL_NOEXCEPT(
-							noexcept(
-								kerbal::memory::uninitialized_value_construct(
-										&kerbal::utility::declthis<__rawst_agent>()->raw_value()[0],
-										&kerbal::utility::declthis<__rawst_agent>()->raw_value()[N]
-								)
-							)
+							noexcept(kerbal::memory::construct_at(kerbal::utility::declthis<this_type>()->raw_pointer()))
 						)
 				{
-					kerbal::memory::uninitialized_value_construct(this->raw_value() + 0, this->raw_value() + N);
+					kerbal::memory::construct_at(this->raw_pointer());
 				}
 
+				KERBAL_CONSTEXPR14
 				void construct(const_reference src)
+						KERBAL_CONDITIONAL_NOEXCEPT(
+								noexcept(kerbal::memory::construct_at(kerbal::utility::declthis<this_type>()->raw_pointer(), src))
+						)
 				{
-					kerbal::memory::uninitialized_copy(src + 0, src + N, this->raw_value() + 0);
+					kerbal::memory::construct_at(this->raw_pointer(), src);
 				}
 
 				template <typename Up>
+				KERBAL_CONSTEXPR14
 				void construct(const Up (&src)[N])
+						KERBAL_CONDITIONAL_NOEXCEPT(
+								noexcept(kerbal::memory::construct_at(kerbal::utility::declthis<this_type>()->raw_pointer(), src))
+						)
 				{
-					kerbal::memory::uninitialized_copy(src + 0, src + N, this->raw_value() + 0);
+					kerbal::memory::construct_at(this->raw_pointer(), src);
 				}
 
 #		if __cplusplus >= 201103L
 
+				KERBAL_CONSTEXPR14
 				void construct(rvalue_reference src)
+						KERBAL_CONDITIONAL_NOEXCEPT(
+								noexcept(kerbal::memory::construct_at(kerbal::utility::declthis<this_type>()->raw_pointer(), kerbal::compatibility::move(src)))
+						)
 				{
-					kerbal::memory::uninitialized_move(src + 0, src + N, this->raw_value() + 0);
+					kerbal::memory::construct_at(this->raw_pointer(), kerbal::compatibility::move(src));
 				}
 
 				template <typename Up>
+				KERBAL_CONSTEXPR14
 				void construct(Up (&&src)[N])
+						KERBAL_CONDITIONAL_NOEXCEPT(
+								noexcept(kerbal::memory::construct_at(kerbal::utility::declthis<this_type>()->raw_pointer(), kerbal::compatibility::move(src)))
+						)
 				{
-					kerbal::memory::uninitialized_move(src + 0, src + N, this->raw_value() + 0);
+					kerbal::memory::construct_at(this->raw_pointer(), kerbal::compatibility::move(src));
 				}
 
 #		endif
-
-				void destroy() KERBAL_NOEXCEPT
-				{
-					kerbal::memory::destroy_at(this->raw_pointer());
-				}
 
 		};
 
 
 
 		template <typename Type>
-		class raw_storage: public kerbal::data_struct::__rawst_agent<Type>
+		class raw_storage: public kerbal::data_struct::_K_rawst_agent<Type>
 		{
+			private:
+				typedef kerbal::data_struct::_K_rawst_agent<Type> super;
+
 			public:
 				typedef Type						value_type;
 				typedef const value_type			const_type;
@@ -519,10 +412,9 @@ namespace kerbal
 
 			public:
 
-				KERBAL_CONSTEXPR
-				raw_storage() KERBAL_NOEXCEPT
-				{
-				}
+#		if __cplusplus >= 201103L
+				using super::super;
+#		endif
 
 		};
 
