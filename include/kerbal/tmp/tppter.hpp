372fee07d5f4e7b8c1045ff5f2955039c9d5d0d2
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2021-12-07 17:04:16 +0800

    新增 tppter

---
+++ include/kerbal/tmp/tppter.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file       tppter.hpp
+ * @brief
+ * @date       2021-12-07
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_TMP_TPPTER_HPP
+#define KERBAL_TMP_TPPTER_HPP
+
+namespace kerbal
+{
+
+	namespace tmp
+	{
+
+		/**
+		 * type parameter pack terminator
+		 */
+		struct tppter
+		{
+		};
+
+	} // namespace tmp
+
+} // namespace kerbal
+
+#endif // KERBAL_TPPTER_HPP
