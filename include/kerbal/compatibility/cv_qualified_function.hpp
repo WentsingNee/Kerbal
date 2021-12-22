379e402eb3638b33b9ed565ddb9d6255c79811ff
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2021-12-22 23:49:34 +0800

    新增 cv_qualified_function

---
+++ include/kerbal/compatibility/cv_qualified_function.hpp
@@ -0,0 +1,38 @@
+/**
+ * @file       cv_qualified_function.hpp
+ * @brief
+ * @date       2021-12-22
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CV_QUALIFIED_FUNCTION_HPP
+#define KERBAL_CV_QUALIFIED_FUNCTION_HPP
+
+#ifndef KERBAL_HAS_CV_QUALIFIED_FUNCTION_SUPPORT
+
+#	if __cplusplus < 201103L
+
+#		include <kerbal/config/compiler_id.hpp>
+#		include <kerbal/config/compiler_private.hpp>
+
+#		if KERBAL_COMPILER_ID == KERBAL_COMPILER_ID_MSVC
+#			if !KERBAL_MSVC_VERSION_MEETS(18, 0, 0)
+#				define KERBAL_HAS_CV_QUALIFIED_FUNCTION_SUPPORT 0
+#			endif
+#		endif
+
+#		ifndef KERBAL_HAS_CV_QUALIFIED_FUNCTION_SUPPORT
+#			define KERBAL_HAS_CV_QUALIFIED_FUNCTION_SUPPORT 1
+#		endif
+
+#	else
+#		define KERBAL_HAS_CV_QUALIFIED_FUNCTION_SUPPORT 1
+#	endif
+
+#endif
+
+#endif // KERBAL_CV_QUALIFIED_FUNCTION_HPP
