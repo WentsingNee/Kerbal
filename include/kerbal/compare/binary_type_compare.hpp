1cbbf3b3295b2a73b528c4033e9b2d31d76cea0c
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2022-05-12 21:35:56 +0800

    binary_type_compare 支持 void 特化

--- include/kerbal/compare/binary_type_compare.hpp
+++ include/kerbal/compare/binary_type_compare.hpp
@@ -15,6 +15,9 @@
 #include <kerbal/compatibility/constexpr.hpp>
 #include <kerbal/compatibility/noexcept.hpp>
 
+#if __cplusplus >= 201103L
+#	include <kerbal/utility/forward.hpp>
+#endif
 
 namespace kerbal
 {
@@ -22,65 +25,152 @@ namespace kerbal
 	namespace compare
 	{
 
-		template <typename Tp, typename Up>
-		struct binary_type_equal_to
-		{
-				KERBAL_CONSTEXPR
-				bool operator()(const Tp & x, const Up & y) const KERBAL_CONDITIONAL_NOEXCEPT(noexcept(x == y))
-				{
-					return x == y;
-				}
+#define DEF_FWD(NAME) \
+		template <typename T, typename U> \
+		struct NAME;
+
+
+#define DEF_PLAIN(NAME, OP) \
+		template <typename T, typename U> \
+		struct NAME \
+		{ \
+				KERBAL_CONSTEXPR \
+				bool operator()(const T & lhs, const U & rhs) const \
+						KERBAL_CONDITIONAL_NOEXCEPT( \
+								noexcept(static_cast<bool>(lhs OP rhs)) \
+						) \
+				{ \
+					return static_cast<bool>(lhs OP rhs); \
+				} \
 		};
 
-		template <typename Tp, typename Up>
-		struct binary_type_not_equal_to
-		{
-				KERBAL_CONSTEXPR
-				bool operator()(const Tp & x, const Up & y) const KERBAL_CONDITIONAL_NOEXCEPT(noexcept(x != y))
-				{
-					return x != y;
-				}
+
+#	if __cplusplus < 201103L
+
+#define DEF_VOID_1(NAME, OP) \
+		template <typename U> \
+		struct NAME<void, U> \
+		{ \
+ \
+				template <typename T> \
+				bool operator()(const T & lhs, const U & rhs) const \
+				{ \
+					return static_cast<bool>(lhs OP rhs); \
+				} \
 		};
 
-		template <typename Tp, typename Up>
-		struct binary_type_greater
-		{
-				KERBAL_CONSTEXPR
-				bool operator()(const Tp & x, const Up & y) const KERBAL_CONDITIONAL_NOEXCEPT(noexcept(x > y))
-				{
-					return x > y;
-				}
+#	else
+
+#define DEF_VOID_1(NAME, OP) \
+		template <typename U> \
+		struct NAME<void, U> \
+		{ \
+ \
+				template <typename T> \
+				KERBAL_CONSTEXPR \
+				bool operator()(T && lhs, const U & rhs) const \
+						KERBAL_CONDITIONAL_NOEXCEPT( \
+							noexcept(static_cast<bool>(kerbal::utility::forward<T>(lhs) OP rhs)) \
+						) \
+				{ \
+					return static_cast<bool>(kerbal::utility::forward<T>(lhs) OP rhs); \
+				} \
 		};
 
-		template <typename Tp, typename Up>
-		struct binary_type_less
-		{
-				KERBAL_CONSTEXPR
-				bool operator()(const Tp & x, const Up & y) const KERBAL_CONDITIONAL_NOEXCEPT(noexcept(x < y))
-				{
-					return x < y;
-				}
+#	endif
+
+
+#	if __cplusplus < 201103L
+
+#define DEF_VOID_2(NAME, OP) \
+		template <typename T> \
+		struct NAME<T, void> \
+		{ \
+ \
+				template <typename U> \
+				bool operator()(const T & lhs, const U & rhs) const \
+				{ \
+					return static_cast<bool>(lhs OP rhs); \
+				} \
 		};
 
-		template <typename Tp, typename Up>
-		struct binary_type_greater_equal
-		{
-				KERBAL_CONSTEXPR
-				bool operator()(const Tp & x, const Up & y) const KERBAL_CONDITIONAL_NOEXCEPT(noexcept(x >= y))
-				{
-					return x >= y;
-				}
+#	else
+
+#define DEF_VOID_2(NAME, OP) \
+		template <typename T> \
+		struct NAME<T, void> \
+		{ \
+ \
+				template <typename U> \
+				KERBAL_CONSTEXPR \
+				bool operator()(const T & lhs, U && rhs) const \
+						KERBAL_CONDITIONAL_NOEXCEPT( \
+							noexcept(static_cast<bool>(lhs OP kerbal::utility::forward<U>(rhs))) \
+						) \
+				{ \
+					return static_cast<bool>(lhs OP kerbal::utility::forward<U>(rhs)); \
+				} \
 		};
 
-		template <typename Tp, typename Up>
-		struct binary_type_less_equal
-		{
-				KERBAL_CONSTEXPR
-				bool operator()(const Tp & x, const Up & y) const KERBAL_CONDITIONAL_NOEXCEPT(noexcept(x <= y))
-				{
-					return x <= y;
-				}
+#	endif
+
+
+#	if __cplusplus < 201103L
+
+#define DEF_VOID_3(NAME, OP) \
+		template <> \
+		struct NAME<void, void> \
+		{ \
+ \
+				template <typename T, typename U> \
+				bool operator()(const T & lhs, const U & rhs) const \
+				{ \
+					return static_cast<bool>(lhs OP rhs); \
+				} \
 		};
+
+#	else
+
+#define DEF_VOID_3(NAME, OP) \
+		template <> \
+		struct NAME<void, void> \
+		{ \
+ \
+				template <typename T, typename U> \
+				KERBAL_CONSTEXPR \
+				bool operator()(T && lhs, U && rhs) const \
+						KERBAL_CONDITIONAL_NOEXCEPT( \
+							noexcept(static_cast<bool>(kerbal::utility::forward<T>(lhs) OP kerbal::utility::forward<U>(rhs))) \
+						) \
+				{ \
+					return static_cast<bool>(kerbal::utility::forward<T>(lhs) OP kerbal::utility::forward<U>(rhs)); \
+				} \
+		};
+
+#	endif
+
+
+#	define DEF(NAME, OP) \
+		DEF_FWD(NAME) \
+		DEF_PLAIN(NAME, OP) \
+		DEF_VOID_1(NAME, OP) \
+		DEF_VOID_2(NAME, OP) \
+		DEF_VOID_3(NAME, OP)
+
+		DEF(binary_type_equal_to, ==)
+		DEF(binary_type_not_equal_to, !=)
+		DEF(binary_type_less, <)
+		DEF(binary_type_less_equal, <=)
+		DEF(binary_type_greater, >)
+		DEF(binary_type_greater_equal, >=)
+
+#	undef DEF_FWD
+#	undef DEF_PLAIN
+#	undef DEF_VOID_1
+#	undef DEF_VOID_2
+#	undef DEF_VOID_3
+#	undef DEF
+
 
 	} // namespace compare
 
