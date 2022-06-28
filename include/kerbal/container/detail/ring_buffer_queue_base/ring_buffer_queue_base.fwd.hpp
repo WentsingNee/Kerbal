777ec621947b5afd97740095432ff0ab70c22b9e
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2022-06-29 00:10:25 +0800

    新增 ring_buffer_queue

---
+++ include/kerbal/container/detail/ring_buffer_queue_base/ring_buffer_queue_base.fwd.hpp
@@ -0,0 +1,34 @@
+/**
+ * @file       ring_buffer_queue_base.fwd.hpp
+ * @brief
+ * @date       2022-03-30
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_DETAIL_RING_BUFFER_QUEUE_BASE_RING_BUFFER_QUEUE_BASE_FWD_HPP
+#define KERBAL_CONTAINER_DETAIL_RING_BUFFER_QUEUE_BASE_RING_BUFFER_QUEUE_BASE_FWD_HPP
+
+
+namespace kerbal
+{
+
+	namespace container
+	{
+
+		namespace detail
+		{
+
+			template <typename T>
+			class rbq_allocator_unrelated;
+
+		} // namespace detail
+
+	} // namespace container
+
+} // namespace kerbal
+
+#endif // KERBAL_CONTAINER_DETAIL_RING_BUFFER_QUEUE_BASE_RING_BUFFER_QUEUE_BASE_FWD_HPP
