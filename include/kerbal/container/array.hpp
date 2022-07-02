1794188275b6645b21a28ca017e7f4b75f6e85b0
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2022-07-02 21:58:06 +0800

    优化 array 目录结构

---
+++ include/kerbal/container/array.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file       array.hpp
+ * @brief
+ * @date       2022-07-02
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_ARRAY_HPP
+#define KERBAL_CONTAINER_ARRAY_HPP
+
+#include <kerbal/container/array/array.fwd.hpp>
+#include <kerbal/container/array/array.decl.hpp>
+#include <kerbal/container/array/array.impl.hpp>
+
+#endif // KERBAL_CONTAINER_ARRAY_HPP
