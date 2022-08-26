20994a600d7dadbd4c55a1c060f82718c33b8e97
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2022-08-26 10:34:25 +0800

    优化 static_vector 目录结构

---
+++ include/kerbal/container/static_vector.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file       static_vector.hpp
+ * @brief
+ * @date       2022-08-26
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_STATIC_VECTOR_HPP
+#define KERBAL_CONTAINER_STATIC_VECTOR_HPP
+
+#include <kerbal/container/static_vector/static_vector.fwd.hpp>
+#include <kerbal/container/static_vector/static_vector.decl.hpp>
+#include <kerbal/container/static_vector/static_vector.impl.hpp>
+
+#endif // KERBAL_CONTAINER_STATIC_VECTOR_HPP
---
+++ include/kerbal/container/static_vector/static_vector.fwd.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file       static_vector.fwd.hpp
+ * @brief
+ * @date       2022-08-26
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_STATIC_VECTOR_STATIC_VECTOR_FWD_HPP
+#define KERBAL_CONTAINER_STATIC_VECTOR_STATIC_VECTOR_FWD_HPP
+
+#include <cstddef>
+
+
+namespace kerbal
+{
+
+	namespace container
+	{
+
+		template <typename Tp, std::size_t N>
+		class static_vector;
+
+	} // namespace container
+
+} // namespace kerbal
+
+#endif // KERBAL_CONTAINER_STATIC_VECTOR_STATIC_VECTOR_FWD_HPP
