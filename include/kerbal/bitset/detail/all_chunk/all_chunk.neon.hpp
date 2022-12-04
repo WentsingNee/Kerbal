429bb8ea15f478ee1769e566d504c03a51d31761
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2022-12-04 16:31:05 +0800

    all_chunk

--- include/kerbal/bitset/detail/all_chunk/all_chunk.neon.hpp
+++ include/kerbal/bitset/detail/all_chunk/all_chunk.neon.hpp
@@ -44,7 +44,7 @@ namespace kerbal
 					return (vgetq_lane_u64(a, 0) & vgetq_lane_u64(a, 1)) == ALL_ONE::value;
 				}
 
-				inline bool all_chunk(const kerbal::compatibility::uint32_t block[], std::size_t trunk_size) KERBAL_NOEXCEPT
+				inline bool all_chunk(const kerbal::compatibility::uint32_t block[], std::size_t chunk_size) KERBAL_NOEXCEPT
 				{
 					typedef kerbal::compatibility::uint32_t Block;
 					typedef kerbal::type_traits::integral_constant<Block, static_cast<Block>(~static_cast<Block>(0))> ALL_ONE;
@@ -52,59 +52,7 @@ namespace kerbal
 					typedef kerbal::type_traits::integral_constant<std::size_t, 128 / 32> STEP;
 
 					std::size_t i = 0;
-					for (; i + STEP::value <= trunk_size; i += STEP::value) {
-						uint64x2_t q_block = vreinterpretq_u64_u32(vld1q_u32(&block[i]));
-						if (!test_all_ones(q_block)) {
-							return false;
-						}
-					}
-
-					while (i < trunk_size) {
-						EACH(i);
-						++i;
-					}
-
-					return true;
-				}
-
-				inline bool all_chunk4(const kerbal::compatibility::uint32_t block[], std::size_t trunk_size) KERBAL_NOEXCEPT
-				{
-					typedef kerbal::compatibility::uint32_t Block;
-					typedef kerbal::type_traits::integral_constant<Block, static_cast<Block>(~static_cast<Block>(0))> ALL_ONE;
-
-					typedef kerbal::type_traits::integral_constant<std::size_t, 128 / 32> STEP;
-					const uint32x4_t q_ALL_ONE = vdupq_n_u32(ALL_ONE::value);
-
-					std::size_t i = 0;
-					for (; i + 4 * STEP::value <= trunk_size; i += 4 * STEP::value) {
-						uint32x4x4_t qx4_block = vld4q_u32(&block[i]);
-
-						qx4_block.val[0] = vandq_u32(qx4_block.val[0], qx4_block.val[1]);
-						qx4_block.val[0] = vandq_u32(qx4_block.val[0], qx4_block.val[2]);
-						qx4_block.val[0] = vandq_u32(qx4_block.val[0], qx4_block.val[3]);
-
-						if (!test_all_ones(vreinterpretq_u64_u32(qx4_block.val[0]))) {
-							return false;
-						}
-					}
-
-					while (i < trunk_size) {
-						EACH(i);
-						++i;
-					}
-
-					return true;
-				}
-
-				inline bool all_chunk8(const kerbal::compatibility::uint32_t block[], std::size_t trunk_size) KERBAL_NOEXCEPT
-				{
-					typedef kerbal::compatibility::uint32_t Block;
-					typedef kerbal::type_traits::integral_constant<Block, static_cast<Block>(~static_cast<Block>(0))> ALL_ONE;
-
-					typedef kerbal::type_traits::integral_constant<std::size_t, 128 / 32> STEP;
-
-					std::size_t i = 0;
-					for (; i + 8 * STEP::value <= trunk_size; i += 8 * STEP::value) {
+					for (; i + 8 * STEP::value <= chunk_size; i += 8 * STEP::value) {
 //						uint64x2_t q_blockp0 = vreinterpretq_u64_u32(vld1q_u32(&block[i + 0 * STEP::value]));
 //						uint64x2_t q_blockp1 = vreinterpretq_u64_u32(vld1q_u32(&block[i + 1 * STEP::value]));
 //						uint64x2_t q_blockp2 = vreinterpretq_u64_u32(vld1q_u32(&block[i + 2 * STEP::value]));
@@ -145,7 +93,51 @@ namespace kerbal
 						}
 					}
 
-					while (i < trunk_size) {
+					while (i < chunk_size) {
+						EACH(i);
+						++i;
+					}
+
+					return true;
+				}
+
+				inline bool all_chunk(const kerbal::compatibility::uint64_t block[], std::size_t chunk_size) KERBAL_NOEXCEPT
+				{
+					typedef kerbal::compatibility::uint64_t Block;
+					typedef kerbal::type_traits::integral_constant<Block, static_cast<Block>(~static_cast<Block>(0))> ALL_ONE;
+
+					typedef kerbal::type_traits::integral_constant<std::size_t, 128 / 64> STEP;
+
+					std::size_t i = 0;
+					for (; i + 8 * STEP::value <= chunk_size; i += 8 * STEP::value) {
+
+						uint32x4x4_t qx4_block0 = vld4q_u32(reinterpret_cast<const uint32_t *>(&block[i])); // arm g++ doesn't support vld4q_u64
+						uint32x4x4_t qx4_block1 = vld4q_u32(reinterpret_cast<const uint32_t *>(&block[i + 4 * STEP::value]));
+
+						uint64x2_t q_blockp0 = vreinterpretq_u64_u32(qx4_block0.val[0]);
+						uint64x2_t q_blockp1 = vreinterpretq_u64_u32(qx4_block0.val[1]);
+						uint64x2_t q_blockp2 = vreinterpretq_u64_u32(qx4_block0.val[2]);
+						uint64x2_t q_blockp3 = vreinterpretq_u64_u32(qx4_block0.val[3]);
+						uint64x2_t q_blockp4 = vreinterpretq_u64_u32(qx4_block1.val[0]);
+						uint64x2_t q_blockp5 = vreinterpretq_u64_u32(qx4_block1.val[1]);
+						uint64x2_t q_blockp6 = vreinterpretq_u64_u32(qx4_block1.val[2]);
+						uint64x2_t q_blockp7 = vreinterpretq_u64_u32(qx4_block1.val[3]);
+
+						q_blockp0 = vandq_u64(q_blockp0, q_blockp1);
+						q_blockp0 = vandq_u64(q_blockp0, q_blockp2);
+						q_blockp0 = vandq_u64(q_blockp0, q_blockp3);
+
+						q_blockp0 = vandq_u64(q_blockp0, q_blockp4);
+						q_blockp0 = vandq_u64(q_blockp0, q_blockp5);
+						q_blockp0 = vandq_u64(q_blockp0, q_blockp6);
+						q_blockp0 = vandq_u64(q_blockp0, q_blockp7);
+
+						if (!test_all_ones(q_blockp0)) {
+							return false;
+						}
+					}
+
+					while (i < chunk_size) {
 						EACH(i);
 						++i;
 					}
