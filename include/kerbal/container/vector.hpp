a03570b8bd7bf0bcf03b1e130fa65cf71dcc1bc1
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2023-04-03 23:28:43 +0800

    module 实验

--- include/kerbal/container/vector.hpp
+++ include/kerbal/container/vector.hpp
@@ -12,8 +12,27 @@
 #ifndef KERBAL_CONTAINER_VECTOR_HPP
 #define KERBAL_CONTAINER_VECTOR_HPP
 
+
+#include <kerbal/config/modules/modules.hpp>
+
+#if KERBAL_ENABLE_MODULES
+module;
+#endif
+
+
+#if KERBAL_ENABLE_MODULES
+export module kerbal.container.vector;
+#endif
+
+
+#if KERBAL_ENABLE_MODULES
+export import kerbal.container.vector.fwd;
+export import kerbal.container.vector.decl;
+export import kerbal.container.vector.impl;
+#else
 #include <kerbal/container/vector/vector.fwd.hpp>
 #include <kerbal/container/vector/vector.decl.hpp>
 #include <kerbal/container/vector/vector.impl.hpp>
+#endif
 
 #endif // KERBAL_CONTAINER_VECTOR_HPP
