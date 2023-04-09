a062b6542978d4102495cd2452912c5187bc15df
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2023-04-09 17:55:58 +0800

    mt_twist 支持 wasm_simd128 (early)

--- include/kerbal/random/detail/mt_twist/mt_twist.wasm_simd128.hpp
+++ include/kerbal/random/detail/mt_twist/mt_twist.wasm_simd128.hpp
@@ -48,56 +48,78 @@ namespace kerbal
 
 					typedef kerbal::type_traits::integral_constant<std::size_t, 128 / 32> STEP;
 					const v128_t v128_UPPER_MASK = wasm_u32x4_const_splat(static_cast<int>(UPPER_MASK::value));
-//					const v128_t v128_ZERO = wasm_u32x4_const_splat(0);
 					const v128_t v128_ONE = wasm_u32x4_const_splat(1);
 					const v128_t v128_A = wasm_u32x4_const_splat(static_cast<int>(A));
 
 					unsigned int i = 0; // Note: gcc unreasonable warning: iteration 4611686018427387903 invokes undefined behavior [-Waggressive-loop-optimizations]
 					for (; i + STEP::value <= NPM::value; i += STEP::value) {
-						v128_t v128_mti = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i]));
-						v128_t v128_mtip1 = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i + 1]));
+						v128_t v128_mti = wasm_v128_load(&mt[i]);
+						v128_t v128_mtip1 = wasm_v128_load(&mt[i + 1]);
 						v128_t v128_y = wasm_v128_bitselect(v128_mti, v128_mtip1, v128_UPPER_MASK);
 						v128_t v128_mag_mask = wasm_v128_and(v128_y, v128_ONE);
-//						v128_mag_mask = wasm_i32x4_sub(v128_ZERO, v128_mag_mask); // <=> wasm_i32x4_eq(v128_mag_mask, v128_ONE)
 						v128_mag_mask = wasm_i32x4_eq(v128_mag_mask, v128_ONE);
 						v128_mag_mask = wasm_v128_and(v128_mag_mask, v128_A);
 						v128_y = wasm_u32x4_shr(v128_y, 1);
 						v128_y = wasm_v128_xor(v128_y, v128_mag_mask);
-						v128_t v128_mtipm = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i + M]));
+						v128_t v128_mtipm = wasm_v128_load(&mt[i + M]);
 						v128_mti = wasm_v128_xor(v128_y, v128_mtipm);
-						wasm_v128_store(reinterpret_cast<v128_t*>(&mt[i]), v128_mti);
+						wasm_v128_store(&mt[i], v128_mti);
 					}
 
 					typedef kerbal::type_traits::integral_constant<int, NPM::value % STEP::value> FIRST_STEP_REMAIN;
 					if (FIRST_STEP_REMAIN::value == 3) {
-//						v128_t v128_mti = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i]));
-//						v128_t v128_mtip1 = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i + 1]));
+//						v128_t v128_mti = wasm_v128_load(&mt[i]); // FIRST_STEP_REMAIN::value (3) + M >= STEP::value always true
+//
+//						v128_t v128_mtip1;
+//						if (FIRST_STEP_REMAIN::value + M - 1 >= STEP::value) {
+//							v128_mtip1 = wasm_v128_load(&mt[i + 1]);
+//						} else {
+//							v128_mtip1 = wasm_v128_load32_lane(&mt[i + 1], v128_mtip1, 0);
+//							v128_mtip1 = wasm_v128_load32_lane(&mt[i + 2], v128_mtip1, 1);
+//							v128_mtip1 = wasm_v128_load32_lane(&mt[i + 3], v128_mtip1, 2);
+//						}
+//
+//
+//
 //						v128_t v128_y = wasm_v128_bitselect(v128_mti, v128_mtip1, v128_UPPER_MASK);
 //						v128_t v128_mag_mask = wasm_v128_and(v128_y, v128_ONE);
-//						v128_mag_mask = wasm_i32x4_sub(v128_ZERO, v128_mag_mask); // <=> wasm_i32x4_eq(v128_mag_mask, v128_ONE)
+//						v128_mag_mask = wasm_i32x4_eq(v128_mag_mask, v128_ONE);
 //						v128_mag_mask = wasm_v128_and(v128_mag_mask, v128_A);
 //						v128_y = wasm_u32x4_shr(v128_y, 1);
 //						v128_y = wasm_v128_xor(v128_y, v128_mag_mask);
-//						v128_t v128_mtipm = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i + M]));
+//						v128_t v128_mtipm; {
+//							v128_mtipm = wasm_v128_load32_lane(&mt[i + M + 0], v128_mtipm, 0);
+//							v128_mtipm = wasm_v128_load32_lane(&mt[i + M + 1], v128_mtipm, 1);
+//							v128_mtipm = wasm_v128_load32_lane(&mt[i + M + 2], v128_mtipm, 2);
+//						}
 //						v128_mti = wasm_v128_xor(v128_y, v128_mtipm);
-//						_mm_storel_epi64(reinterpret_cast<v128_t*>(&mt[i]), v128_mti);
+//
+//						if (true) {
+//							kerbal::compatibility::uint32_t t = mt[i + 3];
+//							wasm_v128_store(&mt[i], v128_mti);
+//							// FIRST_STEP_REMAIN::value (3) + M >= STEP::value always true
+//							mt[i + 3] = t;
+//						} else {
+//							wasm_v128_store32_lane(&mt[i + 0], v128_mti, 0);
+//							wasm_v128_store32_lane(&mt[i + 1], v128_mti, 1);
+//							wasm_v128_store32_lane(&mt[i + 2], v128_mti, 2);
+//						}
+
 						EACH1(i);
 						EACH1(i+1);
-
-						i += 2;
-						EACH1(i);
+						EACH1(i+2);
 					} else if (FIRST_STEP_REMAIN::value == 2) {
-//						v128_t v128_mti = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i]));
-//						v128_t v128_mtip1 = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i + 1]));
+//						v128_t v128_mti = _mm_loadl_epi64(&mt[i]);
+//						v128_t v128_mtip1 = _mm_loadl_epi64(&mt[i + 1]);
 //						v128_t v128_y = wasm_v128_bitselect(v128_mti, v128_mtip1, v128_UPPER_MASK);
 //						v128_t v128_mag_mask = wasm_v128_and(v128_y, v128_ONE);
 //						v128_mag_mask = wasm_i32x4_sub(v128_ZERO, v128_mag_mask); // <=> wasm_i32x4_eq(v128_mag_mask, v128_ONE)
 //						v128_mag_mask = wasm_v128_and(v128_mag_mask, v128_A);
 //						v128_y = wasm_u32x4_shr(v128_y, 1);
 //						v128_y = wasm_v128_xor(v128_y, v128_mag_mask);
-//						v128_t v128_mtipm = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i + M]));
+//						v128_t v128_mtipm = _mm_loadl_epi64(&mt[i + M]));
 //						v128_mti = wasm_v128_xor(v128_y, v128_mtipm);
-//						_mm_storel_epi64(reinterpret_cast<v128_t*>(&mt[i]), v128_mti);
+//						_mm_storel_epi64(&mt[i]), v128_mti);
 						EACH1(i);
 					} else if (FIRST_STEP_REMAIN::value == 1) {
 						EACH1(i);
@@ -106,50 +128,49 @@ namespace kerbal
 					i = NPM::value;
 
 					for (; i + STEP::value <= N - 1; i += STEP::value) {
-						v128_t v128_mti = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i]));
-						v128_t v128_mtip1 = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i + 1]));
+						v128_t v128_mti = wasm_v128_load(&mt[i]);
+						v128_t v128_mtip1 = wasm_v128_load(&mt[i + 1]);
 						v128_t v128_y = wasm_v128_bitselect(v128_mti, v128_mtip1, v128_UPPER_MASK);
 						v128_t v128_mag_mask = wasm_v128_and(v128_y, v128_ONE);
-//						v128_mag_mask = wasm_i32x4_sub(v128_ZERO, v128_mag_mask); // <=> wasm_i32x4_eq(v128_mag_mask, v128_ONE)
 						v128_mag_mask = wasm_i32x4_eq(v128_mag_mask, v128_ONE);
 						v128_mag_mask = wasm_v128_and(v128_mag_mask, v128_A);
 						v128_y = wasm_u32x4_shr(v128_y, 1);
 						v128_y = wasm_v128_xor(v128_y, v128_mag_mask);
-						v128_t v128_mtipm = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i - (NPM::value)]));
+						v128_t v128_mtipm = wasm_v128_load(&mt[i - (NPM::value)]);
 						v128_mti = wasm_v128_xor(v128_y, v128_mtipm);
-						wasm_v128_store(reinterpret_cast<v128_t*>(&mt[i]), v128_mti);
+						wasm_v128_store(&mt[i], v128_mti);
 					}
 
 					typedef kerbal::type_traits::integral_constant<int, (M - 1) % STEP::value> SECOND_STEP_REMAIN;
 					if (SECOND_STEP_REMAIN::value == 3) {
-//						v128_t v128_mti = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i]));
-//						v128_t v128_mtip1 = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i + 1]));
+//						v128_t v128_mti = _mm_loadl_epi64(&mt[i]);
+//						v128_t v128_mtip1 = _mm_loadl_epi64(&mt[i + 1]);
 //						v128_t v128_y = wasm_v128_bitselect(v128_mti, v128_mtip1, v128_UPPER_MASK);
 //						v128_t v128_mag_mask = wasm_v128_and(v128_y, v128_ONE);
 //						v128_mag_mask = wasm_i32x4_sub(v128_ZERO, v128_mag_mask); // <=> wasm_i32x4_eq(v128_mag_mask, v128_ONE)
 //						v128_mag_mask = wasm_v128_and(v128_mag_mask, v128_A);
 //						v128_y = wasm_u32x4_shr(v128_y, 1);
 //						v128_y = wasm_v128_xor(v128_y, v128_mag_mask);
-//						v128_t v128_mtipm = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i - (NPM::value)]));
+//						v128_t v128_mtipm = _mm_loadl_epi64(&mt[i - (NPM::value)]));
 //						v128_mti = wasm_v128_xor(v128_y, v128_mtipm);
-//						_mm_storel_epi64(reinterpret_cast<v128_t*>(&mt[i]), v128_mti);
+//						_mm_storel_epi64(&mt[i]), v128_mti);
 						EACH2(i);
 						EACH2(i+1);
 
 						i += 2;
 						EACH2(i);
 					} else if (SECOND_STEP_REMAIN::value == 2) {
-//						v128_t v128_mti = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i]));
-//						v128_t v128_mtip1 = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i + 1]));
+//						v128_t v128_mti = _mm_loadl_epi64(&mt[i]);
+//						v128_t v128_mtip1 = _mm_loadl_epi64(&mt[i + 1]);
 //						v128_t v128_y = wasm_v128_bitselect(v128_mti, v128_mtip1, v128_UPPER_MASK);
 //						v128_t v128_mag_mask = wasm_v128_and(v128_y, v128_ONE);
 //						v128_mag_mask = wasm_i32x4_sub(v128_ZERO, v128_mag_mask); // <=> wasm_i32x4_eq(v128_mag_mask, v128_ONE)
 //						v128_mag_mask = wasm_v128_and(v128_mag_mask, v128_A);
 //						v128_y = wasm_u32x4_shr(v128_y, 1);
 //						v128_y = wasm_v128_xor(v128_y, v128_mag_mask);
-//						v128_t v128_mtipm = _mm_loadl_epi64(reinterpret_cast<const v128_t*>(&mt[i - (NPM::value)]));
+//						v128_t v128_mtipm = _mm_loadl_epi64(&mt[i - (NPM::value)]));
 //						v128_mti = wasm_v128_xor(v128_y, v128_mtipm);
-//						_mm_storel_epi64(reinterpret_cast<v128_t*>(&mt[i]), v128_mti);
+//						_mm_storel_epi64(&mt[i]), v128_mti);
 
 						EACH2(i);
 					} else if (SECOND_STEP_REMAIN::value == 1) {
@@ -180,17 +201,17 @@ namespace kerbal
 
 					std::size_t i = 0;
 					for (; i + STEP::value <= NPM::value; i += STEP::value) {
-						v128_t v128_mti = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i]));
-						v128_t v128_mtip1 = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i + 1]));
+						v128_t v128_mti = wasm_v128_load(&mt[i]);
+						v128_t v128_mtip1 = wasm_v128_load(&mt[i + 1]);
 						v128_t v128_y = wasm_v128_bitselect(v128_mti, v128_mtip1, v128_UPPER_MASK);
 						v128_t v128_mag_mask = wasm_v128_and(v128_y, v128_ONE);
 						v128_mag_mask = wasm_i64x2_sub(v128_ZERO, v128_mag_mask); // <=> wasm_i64x2_eq(v128_mag_mask, v128_ONE)SSE4.1
 						v128_mag_mask = wasm_v128_and(v128_mag_mask, v128_A);
 						v128_y = wasm_u64x2_shr(v128_y, 1);
 						v128_y = wasm_v128_xor(v128_y, v128_mag_mask);
-						v128_t v128_mtipm = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i + M]));
+						v128_t v128_mtipm = wasm_v128_load(&mt[i + M]);
 						v128_mti = wasm_v128_xor(v128_y, v128_mtipm);
-						wasm_v128_store(reinterpret_cast<v128_t*>(&mt[i]), v128_mti);
+						wasm_v128_store(&mt[i], v128_mti);
 					}
 
 					typedef kerbal::type_traits::integral_constant<long long, NPM::value % STEP::value> FIRST_STEP_REMAIN;
@@ -201,17 +222,17 @@ namespace kerbal
 					i = NPM::value;
 
 					for (; i + STEP::value <= N - 1; i += STEP::value) {
-						v128_t v128_mti = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i]));
-						v128_t v128_mtip1 = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i + 1]));
+						v128_t v128_mti = wasm_v128_load(&mt[i]);
+						v128_t v128_mtip1 = wasm_v128_load(&mt[i + 1]);
 						v128_t v128_y = wasm_v128_bitselect(v128_mti, v128_mtip1, v128_UPPER_MASK);
 						v128_t v128_mag_mask = wasm_v128_and(v128_y, v128_ONE);
 						v128_mag_mask = wasm_i64x2_sub(v128_ZERO, v128_mag_mask); // <=> wasm_i64x2_eq(v128_mag_mask, v128_ONE)SSE4.1
 						v128_mag_mask = wasm_v128_and(v128_mag_mask, v128_A);
 						v128_y = wasm_u64x2_shr(v128_y, 1);
 						v128_y = wasm_v128_xor(v128_y, v128_mag_mask);
-						v128_t v128_mtipm = wasm_v128_load(reinterpret_cast<const v128_t*>(&mt[i - (NPM::value)]));
+						v128_t v128_mtipm = wasm_v128_load(&mt[i - (NPM::value)]);
 						v128_mti = wasm_v128_xor(v128_y, v128_mtipm);
-						wasm_v128_store(reinterpret_cast<v128_t*>(&mt[i]), v128_mti);
+						wasm_v128_store(&mt[i], v128_mti);
 					}
 
 					typedef kerbal::type_traits::integral_constant<long long, (M - 1) % STEP::value> SECOND_STEP_REMAIN;
