abc504958fbf89632720210f6433d83ba9d29149
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2023-10-20 00:35:15 +0800

    tuple 增加注释

--- include/kerbal/utility/tuple/detail/tuple.cxx11.part.hpp
+++ include/kerbal/utility/tuple/detail/tuple.cxx11.part.hpp
@@ -673,6 +673,12 @@ namespace kerbal
 
 			public:
 
+				/*
+				 *  f(kerbal::type_traits::integral_constant<std::size_t, 0>(), t.get<0>());
+				 *  f(kerbal::type_traits::integral_constant<std::size_t, 1>(), t.get<1>());
+				 *  f(kerbal::type_traits::integral_constant<std::size_t, 2>(), t.get<2>());
+				 *  and return the tuple self
+				 */
 				template <typename F>
 				KERBAL_CONSTEXPR14
 				tuple& for_each(F f) &
@@ -761,6 +767,9 @@ namespace kerbal
 
 			public:
 
+				/*
+				 * return f(t.get<0>(), t.get<1>(), t.get<2>(), ...)
+				 */
 				template <typename F>
 				KERBAL_CONSTEXPR14
 				auto apply_to(F f) & ->
@@ -890,6 +899,13 @@ namespace kerbal
 
 			public:
 
+				/*
+				 * return tuple(
+				 * 	     transform(kerbal::type_traits::integral_constant<std::size_t, 0>(), t.get<0>()),
+				 * 	     transform(kerbal::type_traits::integral_constant<std::size_t, 1>(), t.get<1>()),
+				 * 	     transform(kerbal::type_traits::integral_constant<std::size_t, 2>(), t.get<2>()), ...
+				 * 	)
+				 */
 				template <typename F>
 				KERBAL_CONSTEXPR14
 				auto transform(F f) & ->
@@ -938,6 +954,9 @@ namespace kerbal
 
 			public:
 
+				/*
+				 * return tuple(t.get<I1>(), t.get<I2>(), t.get<I3>())
+				 */
 				template <std::size_t ... Index>
 				KERBAL_CONSTEXPR
 				auto select() const & ->
@@ -970,6 +989,9 @@ namespace kerbal
 
 			public:
 
+				/*
+				 * return tuple(t.get<N - 1>(), t.get<N - 2>(), ..., t.get<0>())
+				 */
 				KERBAL_CONSTEXPR
 				auto reverse() const & ->
 						decltype(k_reverse_impl(*this, kerbal::utility::make_index_sequence<TUPLE_SIZE::value>()))
