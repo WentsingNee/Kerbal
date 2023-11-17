acf54e2aff1e382319b46cd82a802937d8fc0a36
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2023-11-17 16:33:04 +0800

    新增 avl_multi_index

---
+++ include/kerbal/container/detail/avl_multi_index_base.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file       avl_multi_index_base.hpp
+ * @brief
+ * @date       2023-11-16
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_DETAIL_AVL_MULTI_INDEX_BASE_HPP
+#define KERBAL_CONTAINER_DETAIL_AVL_MULTI_INDEX_BASE_HPP
+
+#include <kerbal/container/detail/avl_multi_index_base/avl_multi_index_base.fwd.hpp>
+#include <kerbal/container/detail/avl_multi_index_base/avl_multi_index_base.decl.hpp>
+#include <kerbal/container/detail/avl_multi_index_base/avl_multi_index_base.impl.hpp>
+
+#endif // KERBAL_CONTAINER_DETAIL_AVL_MULTI_INDEX_BASE_HPP
