275bccdaedb3762c74ee00806e45b99316544305
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2023-11-17 16:35:08 +0800

    [new-facility] 新增 avl_multiset, avl_multimap

---
+++ include/kerbal/container/avl_multimap.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file       avl_multimap.hpp
+ * @brief
+ * @date       2022-11-21
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_AVL_MULTIMAP_HPP
+#define KERBAL_CONTAINER_AVL_MULTIMAP_HPP
+
+#include <kerbal/container/avl_multimap/avl_multimap.fwd.hpp>
+#include <kerbal/container/avl_multimap/avl_multimap.decl.hpp>
+#include <kerbal/container/avl_multimap/avl_multimap.impl.hpp>
+
+#endif // KERBAL_CONTAINER_AVL_MULTIMAP_HPP
