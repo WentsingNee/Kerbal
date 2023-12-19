81387fb4477718db748b09d75960f7b29930ffdd
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2023-12-19 15:42:47 +0800

    新增 rbtree

---
+++ include/kerbal/container/rb_ordered/rb_ordered.fwd.hpp
@@ -0,0 +1,38 @@
+/**
+ * @file       rb_ordered.fwd.hpp
+ * @brief
+ * @date       2023-12-19
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_RB_ORDERED_RB_ORDERED_FWD_HPP
+#define KERBAL_CONTAINER_RB_ORDERED_RB_ORDERED_FWD_HPP
+
+#include <kerbal/compare/binary_type_compare.hpp>
+
+#include <memory>
+
+
+namespace kerbal
+{
+
+	namespace container
+	{
+
+		template <
+			typename Entity,
+			typename Extract,
+			typename KeyCompare = kerbal::compare::binary_type_less<void, void>,
+			typename Allocator = std::allocator<Entity>
+		>
+		class rb_ordered;
+
+	} // namespace container
+
+} // namespace kerbal
+
+#endif // KERBAL_CONTAINER_RB_ORDERED_RB_ORDERED_FWD_HPP
