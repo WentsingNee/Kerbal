f32ed33d98aa336fbee396330073cc960f9dd377
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-01-23 13:20:22 +0800

    omp

--- include/kerbal/ompalgo/sort/quick_sort.hpp
+++ include/kerbal/ompalgo/sort/quick_sort.hpp
@@ -1,7 +1,7 @@
 /**
  * @file       quick_sort.hpp
  * @brief
- * @date       2020-6-9
+ * @date       2020-06-09
  * @author     Peter
  * @copyright
  *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
@@ -14,12 +14,24 @@
 
 #include <kerbal/openmp/disable_warning.hpp>
 
-#include <kerbal/algorithm/sort/insertion_sort.hpp>
-#include <kerbal/algorithm/sort/quick_sort.hpp>
-#include <kerbal/algorithm/swap.hpp>
-#include <kerbal/iterator/iterator.hpp>
+#include <kerbal/compare/basic_compare.hpp>
+#include <kerbal/iterator/iterator_traits.hpp>
 
 #include <omp.h>
+
+
+#ifdef _OPENMP
+#	include <kerbal/openmp/config/task_directive.hpp>
+
+#	if KERBAL_OMP_HAS_TASK_DIRECTIVE_SUPPORT
+#		include <kerbal/ompalgo/sort/detail/quick_sort/quick_sort.task.part.hpp>
+#	else
+#		include <kerbal/ompalgo/sort/detail/quick_sort/quick_sort.default.hpp>
+#	endif
+#else
+#	include <kerbal/ompalgo/sort/detail/quick_sort/quick_sort.disable.part.hpp>
+#endif
+
 
 namespace kerbal
 {
@@ -27,130 +39,12 @@ namespace kerbal
 	namespace ompalgo
 	{
 
-#if !defined(_OPENMP)
-
-		template <typename BidirectionalIterator, typename Compare>
-		void quick_sort(BidirectionalIterator first, BidirectionalIterator last, Compare cmp)
-		{
-			kerbal::algorithm::quick_sort(first, last, cmp);
-		}
-
-#else
-
-#if _OPENMP > 200203L
-#	define KERBAL_OMP_SUPPORT_TASK_DIRECTIVE 1
-#else
-#	define KERBAL_OMP_SUPPORT_TASK_DIRECTIVE 0
-#endif
-
-#	if KERBAL_OMP_SUPPORT_TASK_DIRECTIVE
-
-		namespace detail
-		{
-
-			template <typename BidirectionalIterator, typename Compare>
-			void quick_sort_task_dis(BidirectionalIterator first, BidirectionalIterator last, Compare cmp)
-			{
-				typedef BidirectionalIterator iterator;
-
-				while (kerbal::iterator::distance_greater_than(first, last, 16)) {
-					if (kerbal::iterator::distance_less_than(first, last, 1024 * 1)) {
-						kerbal::algorithm::quick_sort(first, last, cmp);
-						return;
-					}
-
-					iterator back(kerbal::iterator::prev(last));
-					kerbal::algorithm::detail::quick_sort_select_pivot(first, back, cmp);
-					iterator partition_point(kerbal::algorithm::detail::quick_sort_partition(first, back, *back, cmp));
-
-					if (partition_point != back) {
-#						pragma omp task firstprivate(back, partition_point, last)
-						{
-							if (cmp(*back, *partition_point)) {
-								kerbal::algorithm::iter_swap(back, partition_point);
-							}
-							kerbal::ompalgo::detail::quick_sort_task_dis(kerbal::iterator::next(partition_point), last, cmp);
-						}
-
-					}
-					last = partition_point;
-				}
-				// dist <= 16
-				kerbal::algorithm::directly_insertion_sort(first, last, cmp);
-			}
-
-		} // namespace detail
-
-		template <typename BidirectionalIterator, typename Compare>
-		void quick_sort(BidirectionalIterator first, BidirectionalIterator last, Compare cmp)
-		{
-#			pragma omp parallel
-#			pragma omp single
-			kerbal::ompalgo::detail::quick_sort_task_dis(first, last, cmp);
-#			pragma omp taskwait
-		}
-
-#	else
-
-		template <typename BidirectionalIterator, typename Compare>
-		void quick_sort(BidirectionalIterator first, BidirectionalIterator last, Compare cmp, int level = 0)
-		{
-			typedef BidirectionalIterator iterator;
-
-			if (kerbal::iterator::distance_less_than(first, last, 3)) {
-				kerbal::algorithm::directly_insertion_sort(first, last, cmp);
-				return;
-			}
-
-			if (kerbal::iterator::distance_less_than(first, last, 1024 * 1)) {
-				kerbal::algorithm::quick_sort(first, last, cmp);
-				return;
-			}
-
-			iterator back(kerbal::iterator::prev(last));
-			kerbal::algorithm::detail::quick_sort_select_pivot(first, back, cmp);
-			iterator partition_point(kerbal::algorithm::detail::quick_sort_partition(first, back, *back, cmp));
-
-			if (partition_point != back) {
-				if (cmp(*back, *partition_point)) {
-					kerbal::algorithm::iter_swap(back, partition_point);
-				}
-
-#				pragma omp parallel sections
-				{
-#					pragma omp section
-					{
-						printf("%d 0s\n", level);
-						kerbal::ompalgo::quick_sort(kerbal::iterator::next(partition_point), last, cmp, level + 1);
-						printf("%d 0e\n", level);
-					}
-
-#					pragma omp section
-					{
-						printf("%d 1s\n", level);
-						kerbal::ompalgo::quick_sort(first, partition_point, cmp, level + 1);
-						printf("%d 1e\n", level);
-					}
-				}
-
-			} else {
-				printf("%d ss\n", level);
-				kerbal::ompalgo::quick_sort(first, partition_point, cmp, level + 1);
-				printf("%d se\n", level);
-			}
-		}
-
-#	endif // KERBAL_OMP_SUPPORT_TASK_DIRECTIVE
-
-
-#endif
-
 		template <typename BidirectionalIterator>
 		void quick_sort(BidirectionalIterator first, BidirectionalIterator last)
 		{
 			typedef BidirectionalIterator iterator;
 			typedef typename kerbal::iterator::iterator_traits<iterator>::value_type value_type;
-			kerbal::ompalgo::quick_sort(first, last, std::less<value_type>());
+			kerbal::ompalgo::quick_sort(first, last, kerbal::compare::less<value_type>());
 		}
 
 	} // namespace ompalgo
