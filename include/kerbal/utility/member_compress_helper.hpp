c112c76c69926f2e0a2a8f405fc1c5dfa5100611
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-01-29 15:08:24 +0800

    [coding-opt] 拆分 member_compress_helper

---
+++ include/kerbal/utility/member_compress_helper.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file       member_compress_helper.hpp
+ * @brief
+ * @date       2020-09-20
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_UTILITY_MEMBER_COMPRESS_HELPER_HPP
+#define KERBAL_UTILITY_MEMBER_COMPRESS_HELPER_HPP
+
+#include <kerbal/utility/member_compress_helper/member_compress_helper.fwd.hpp>
+
+#include <kerbal/utility/member_compress_helper/detail/member_compress_helper/member_compress_helper.default.part.hpp>
+#include <kerbal/utility/member_compress_helper/detail/member_compress_helper/member_compress_helper.array.part.hpp>
+#include <kerbal/utility/member_compress_helper/detail/member_compress_helper/member_compress_helper.ref.part.hpp>
+
+#endif // KERBAL_UTILITY_MEMBER_COMPRESS_HELPER_HPP
---
+++ include/kerbal/utility/member_compress_helper/detail/member_compress_helper_impl.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file       member_compress_helper_impl.hpp
+ * @brief
+ * @date       2024-04-21
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_UTILITY_MEMBER_COMPRESS_HELPER_DETAIL_MEMBER_COMPRESS_HELPER_IMPL_HPP
+#define KERBAL_UTILITY_MEMBER_COMPRESS_HELPER_DETAIL_MEMBER_COMPRESS_HELPER_IMPL_HPP
+
+#include <kerbal/utility/member_compress_helper/detail/member_compress_helper_impl/member_compress_helper_impl.compressed.part.hpp>
+#include <kerbal/utility/member_compress_helper/detail/member_compress_helper_impl/member_compress_helper_impl.const.part.hpp>
+#include <kerbal/utility/member_compress_helper/detail/member_compress_helper_impl/member_compress_helper_impl.not_compressed.part.hpp>
+
+#endif // KERBAL_UTILITY_MEMBER_COMPRESS_HELPER_DETAIL_MEMBER_COMPRESS_HELPER_IMPL_HPP
