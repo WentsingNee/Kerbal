56ad505f4824a6d708a6690e7ddaa3bca379116f
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-02-20 11:43:33 +0800

    [simd] reverse 增加 simd 优化

--- include/kerbal/algorithm/modifier/reverse.hpp
+++ include/kerbal/algorithm/modifier/reverse.hpp
@@ -12,10 +12,9 @@
 #ifndef KERBAL_ALGORITHM_MODIFIER_REVERSE_HPP
 #define KERBAL_ALGORITHM_MODIFIER_REVERSE_HPP
 
-#include <kerbal/algorithm/swap.hpp>
+#include <kerbal/algorithm/modifier/detail/reverse/reverse.hpp>
+
 #include <kerbal/compatibility/constexpr.hpp>
-#include <kerbal/iterator/iterator.hpp>
-#include <kerbal/iterator/iterator_traits.hpp>
 
 
 namespace kerbal
@@ -24,46 +23,11 @@ namespace kerbal
 	namespace algorithm
 	{
 
-		namespace detail
-		{
-
-			template <typename BidirectionalIterator>
-			KERBAL_CONSTEXPR14
-			void k_reverse(BidirectionalIterator first, BidirectionalIterator last, std::bidirectional_iterator_tag)
-			{
-				while (first != last) {
-					--last;
-					if (first != last) {
-						kerbal::algorithm::iter_swap(first, last);
-						++first;
-					} else {
-						break;
-					}
-				}
-			}
-
-			template <typename RandomAccessIterator>
-			KERBAL_CONSTEXPR14
-			void k_reverse(RandomAccessIterator first, RandomAccessIterator last, std::random_access_iterator_tag)
-			{
-				typedef RandomAccessIterator iterator;
-				typedef typename kerbal::iterator::iterator_traits<iterator>::difference_type difference_type;
-
-				difference_type dist(kerbal::iterator::distance(first, last) >> 1);
-				for (difference_type i = 0; i < dist; ++i) {
-					--last;
-					kerbal::algorithm::iter_swap(first, last);
-					++first;
-				}
-			}
-
-		} // namespace detail
-
 		template <typename BidirectionalIterator>
 		KERBAL_CONSTEXPR14
 		void reverse(BidirectionalIterator first, BidirectionalIterator last)
 		{
-			kerbal::algorithm::detail::k_reverse(first, last, kerbal::iterator::iterator_category(first));
+			kerbal::algorithm::detail::k_reverse_consteval_dispatch(first, last);
 		}
 
 	} // namespace algorithm
