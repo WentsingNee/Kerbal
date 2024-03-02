d14c8e5cd2da2d284016115485f49507db372c74
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-03-02 15:04:36 +0800

    Build src

---
+++ src/test/test_case.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file       test_case.cpp
+ * @brief
+ * @date       2019-11-29
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#include <kerbal/test/test_case.hpp>
+#include <kerbal/test/test_case/test_case.impl.hpp>
