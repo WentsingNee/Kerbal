3cab97c7dbb2d11f813fdeb671fa730cbb087cab
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-05-01 17:20:06 +0800

    each3

--- include/kerbal/random/detail/mt_twist/mt_twist.avx2.part.hpp
+++ include/kerbal/random/detail/mt_twist/mt_twist.avx2.part.hpp
@@ -22,6 +22,8 @@
 
 #include <immintrin.h>
 
+#include <kerbal/random/detail/mt_twist/mt_twist.plain.part.hpp>
+
 
 namespace kerbal
 {
@@ -152,9 +154,7 @@ namespace kerbal
 					} else {
 
 						// i = N - 1;
-
-						result_type y = (mt[N - 1] & UPPER_MASK::value) | (mt[0] & LOWER_MASK::value);
-						mt[N - 1] = mt[M - 1] ^ (y >> 1) ^ ((y & 0x1UL) ? A : 0);
+						EACH3();
 					}
 
 				}
@@ -281,9 +281,7 @@ namespace kerbal
 					} else {
 
 						// i = N - 1;
-
-						result_type y = (mt[N - 1] & UPPER_MASK::value) | (mt[0] & LOWER_MASK::value);
-						mt[N - 1] = mt[M - 1] ^ (y >> 1) ^ ((y & 0x1UL) ? A : 0);
+						EACH3();
 					}
 
 				}
