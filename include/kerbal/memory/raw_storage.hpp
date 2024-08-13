4bd5c10f219e1860f5773d077f4e937103503a4b
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-08-13 18:52:27 +0800

    [coding-opt] 废除 k_rawst_agent

--- include/kerbal/memory/raw_storage.hpp
+++ include/kerbal/memory/raw_storage.hpp
@@ -46,17 +46,14 @@ namespace kerbal
 	{
 
 		template <typename T>
-		class k_rawst_agent;
-
-		template <typename T>
-		class k_rawst_agent:
+		class raw_storage :
 			public kerbal::memory::detail::k_rawst_base<T>
 		{
 			private:
-				typedef k_rawst_agent this_type;
+				typedef raw_storage this_type;
 				typedef kerbal::memory::detail::k_rawst_base<T> super;
 
-			protected:
+			public:
 				typedef typename super::value_type					value_type;
 				typedef typename super::const_type					const_type;
 				typedef typename super::reference					reference;
@@ -69,11 +66,12 @@ namespace kerbal
 				typedef typename super::const_rvalue_reference		const_rvalue_reference;
 #		endif
 
-			protected:
+			public:
 
 #		if __cplusplus < 201103L
 
 #			define EMPTY
+#			define REMAINF(exp) exp
 #			define LEFT_JOIN_COMMA(exp) , exp
 #			define THEAD_NOT_EMPTY(exp) template <exp>
 #			define TARGS_DECL(i) typename KERBAL_MACRO_CONCAT(Arg, i)
@@ -81,7 +79,7 @@ namespace kerbal
 #			define ARGS_USE(i) KERBAL_MACRO_CONCAT(arg, i)
 #			define FBODY(i) \
 				KERBAL_OPT_PPEXPAND_WITH_COMMA_N(THEAD_NOT_EMPTY, EMPTY, TARGS_DECL, i) \
-				explicit k_rawst_agent(kerbal::utility::in_place_t in_place KERBAL_OPT_PPEXPAND_WITH_COMMA_N(LEFT_JOIN_COMMA, EMPTY, ARGS_DECL, i)) : \
+				explicit raw_storage(kerbal::utility::in_place_t in_place KERBAL_OPT_PPEXPAND_WITH_COMMA_N(LEFT_JOIN_COMMA, EMPTY, ARGS_DECL, i)) : \
 					super(in_place KERBAL_OPT_PPEXPAND_WITH_COMMA_N(LEFT_JOIN_COMMA, EMPTY, ARGS_USE, i)) \
 				{ \
 				} \
@@ -90,6 +88,7 @@ namespace kerbal
 				KERBAL_PPEXPAND_N(FBODY, KERBAL_PPEXPAND_EMPTY_SEPARATOR, 20)
 
 #			undef EMPTY
+#			undef REMAINF
 #			undef LEFT_JOIN_COMMA
 #			undef THEAD_NOT_EMPTY
 #			undef TARGS_DECL
@@ -102,11 +101,19 @@ namespace kerbal
 #		endif
 
 				KERBAL_CONSTEXPR
-				k_rawst_agent() KERBAL_NOEXCEPT
+				raw_storage() KERBAL_NOEXCEPT
 				{
 				}
 
-			public:
+				KERBAL_CONSTEXPR14
+				void default_construct()
+					KERBAL_CONDITIONAL_NOEXCEPT(
+						noexcept(kerbal::memory::default_construct_at(kerbal::utility::declthis<this_type>()->raw_pointer()))
+					)
+				{
+					kerbal::memory::default_construct_at(this->raw_pointer());
+				}
+
 
 #		if __cplusplus < 201103L
 
@@ -200,248 +207,6 @@ namespace kerbal
 
 #		endif
 
-		};
-
-/*
-		template <typename T, std::size_t N>
-		class k_rawst_agent<T[N]>:
-				public kerbal::memory::detail::k_rawst_base<T[N]>
-		{
-			private:
-				typedef k_rawst_agent this_type;
-				typedef kerbal::memory::detail::k_rawst_base<T[N]> super;
-
-			protected:
-				typedef typename super::value_type					value_type;
-				typedef typename super::const_type					const_type;
-				typedef typename super::reference					reference;
-				typedef typename super::const_reference				const_reference;
-				typedef typename super::pointer						pointer;
-				typedef typename super::const_pointer				const_pointer;
-
-#		if __cplusplus >= 201103L
-				typedef typename super::rvalue_reference			rvalue_reference;
-				typedef typename super::const_rvalue_reference		const_rvalue_reference;
-#		endif
-
-			protected:
-
-#		if __cplusplus < 201103L
-
-				explicit k_rawst_agent(kerbal::utility::in_place_t in_place) :
-					super(in_place)
-				{
-				}
-
-				explicit k_rawst_agent(kerbal::utility::in_place_t in_place, const_reference src) :
-					super(in_place, src)
-				{
-				}
-
-				template <typename U>
-				explicit k_rawst_agent(kerbal::utility::in_place_t in_place, const U (&src)[N]) :
-					super(in_place, src)
-				{
-				}
-
-#		else
-				using super::super;
-#		endif
-
-				KERBAL_CONSTEXPR
-				k_rawst_agent() KERBAL_NOEXCEPT
-				{
-				}
-
-			public:
-
-				KERBAL_CONSTEXPR14
-				void construct()
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at(kerbal::utility::declthis<this_type>()->raw_pointer()))
-					)
-				{
-					kerbal::memory::construct_at(this->raw_pointer());
-				}
-
-				template <typename Alloc>
-				KERBAL_CONSTEXPR14
-				void construct_using_allocator(Alloc & alloc)
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at_using_allocator(alloc, kerbal::utility::declthis<this_type>()->raw_pointer()))
-					)
-				{
-					kerbal::memory::construct_at_using_allocator(alloc, this->raw_pointer());
-				}
-
-				KERBAL_CONSTEXPR14
-				void construct(const_reference src)
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at(kerbal::utility::declthis<this_type>()->raw_pointer(), src))
-					)
-				{
-					kerbal::memory::construct_at(this->raw_pointer(), src);
-				}
-
-				template <typename Alloc>
-				KERBAL_CONSTEXPR14
-				void construct_using_allocator(Alloc & alloc, const_reference src)
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at_using_allocator(alloc, kerbal::utility::declthis<this_type>()->raw_pointer(), src))
-					)
-				{
-					kerbal::memory::construct_at_using_allocator(alloc, this->raw_pointer(), src);
-				}
-
-				template <typename U>
-				KERBAL_CONSTEXPR14
-				void construct(const U (&src)[N])
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at(kerbal::utility::declthis<this_type>()->raw_pointer(), src))
-					)
-				{
-					kerbal::memory::construct_at(this->raw_pointer(), src);
-				}
-
-				template <typename Alloc, typename U>
-				KERBAL_CONSTEXPR14
-				void construct_using_allocator(Alloc & alloc, const U (&src)[N])
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at_using_allocator(alloc, kerbal::utility::declthis<this_type>()->raw_pointer(), src))
-					)
-				{
-					kerbal::memory::construct_at_using_allocator(alloc, this->raw_pointer(), src);
-				}
-
-#		if __cplusplus >= 201103L
-
-				KERBAL_CONSTEXPR14
-				void construct(rvalue_reference src)
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at(
-							kerbal::utility::declthis<this_type>()->raw_pointer(),
-							kerbal::compatibility::move(src)
-						))
-					)
-				{
-					kerbal::memory::construct_at(this->raw_pointer(), kerbal::compatibility::move(src));
-				}
-
-				template <typename Alloc>
-				KERBAL_CONSTEXPR14
-				void construct_using_allocator(Alloc & alloc, rvalue_reference src)
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at_using_allocator(
-							alloc,
-							kerbal::utility::declthis<this_type>()->raw_pointer(),
-							kerbal::compatibility::move(src)
-						))
-					)
-				{
-					kerbal::memory::construct_at_using_allocator(alloc, this->raw_pointer(), kerbal::compatibility::move(src));
-				}
-
-				template <typename U>
-				KERBAL_CONSTEXPR14
-				void construct(U (&&src)[N])
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at(
-							kerbal::utility::declthis<this_type>()->raw_pointer(),
-							kerbal::compatibility::move(src)
-						))
-					)
-				{
-					kerbal::memory::construct_at(this->raw_pointer(), kerbal::compatibility::move(src));
-				}
-
-				template <typename Alloc, typename U>
-				KERBAL_CONSTEXPR14
-				void construct_using_allocator(Alloc & alloc, U (&&src)[N])
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::construct_at_using_allocator(
-							alloc,
-							kerbal::utility::declthis<this_type>()->raw_pointer(),
-							kerbal::compatibility::move(src)
-						))
-					)
-				{
-					kerbal::memory::construct_at_using_allocator(alloc, this->raw_pointer(), kerbal::compatibility::move(src));
-				}
-
-#		endif
-
-		};
-*/
-
-
-		template <typename T>
-		class raw_storage :
-			public kerbal::memory::k_rawst_agent<T>
-		{
-			private:
-				typedef raw_storage this_type;
-				typedef kerbal::memory::k_rawst_agent<T> super;
-
-			public:
-				typedef typename super::value_type					value_type;
-				typedef typename super::const_type					const_type;
-				typedef typename super::reference					reference;
-				typedef typename super::const_reference				const_reference;
-				typedef typename super::pointer						pointer;
-				typedef typename super::const_pointer				const_pointer;
-
-#		if __cplusplus >= 201103L
-				typedef typename super::rvalue_reference			rvalue_reference;
-				typedef typename super::const_rvalue_reference		const_rvalue_reference;
-#		endif
-
-			public:
-
-#		if __cplusplus < 201103L
-
-#			define EMPTY
-#			define REMAINF(exp) exp
-#			define LEFT_JOIN_COMMA(exp) , exp
-#			define THEAD_NOT_EMPTY(exp) template <exp>
-#			define TARGS_DECL(i) typename KERBAL_MACRO_CONCAT(Arg, i)
-#			define ARGS_DECL(i) const KERBAL_MACRO_CONCAT(Arg, i) & KERBAL_MACRO_CONCAT(arg, i)
-#			define ARGS_USE(i) KERBAL_MACRO_CONCAT(arg, i)
-#			define FBODY(i) \
-				KERBAL_OPT_PPEXPAND_WITH_COMMA_N(THEAD_NOT_EMPTY, EMPTY, TARGS_DECL, i) \
-				explicit raw_storage(kerbal::utility::in_place_t in_place KERBAL_OPT_PPEXPAND_WITH_COMMA_N(LEFT_JOIN_COMMA, EMPTY, ARGS_DECL, i)) : \
-					super(in_place KERBAL_OPT_PPEXPAND_WITH_COMMA_N(LEFT_JOIN_COMMA, EMPTY, ARGS_USE, i)) \
-				{ \
-				} \
-
-				KERBAL_PPEXPAND_N(FBODY, KERBAL_PPEXPAND_EMPTY_SEPARATOR, 0)
-				KERBAL_PPEXPAND_N(FBODY, KERBAL_PPEXPAND_EMPTY_SEPARATOR, 20)
-
-#			undef EMPTY
-#			undef REMAINF
-#			undef LEFT_JOIN_COMMA
-#			undef THEAD_NOT_EMPTY
-#			undef TARGS_DECL
-#			undef ARGS_DECL
-#			undef ARGS_USE
-#			undef FBODY
-
-#		else
-				using super::super;
-#		endif
-
-				KERBAL_CONSTEXPR
-				raw_storage() KERBAL_NOEXCEPT
-				{
-				}
-
-				KERBAL_CONSTEXPR14
-				void default_construct()
-					KERBAL_CONDITIONAL_NOEXCEPT(
-						noexcept(kerbal::memory::default_construct_at(kerbal::utility::declthis<this_type>()->raw_pointer()))
-					)
-				{
-					kerbal::memory::default_construct_at(this->raw_pointer());
-				}
 
 				template <typename Alloc>
 				KERBAL_CONSTEXPR14
