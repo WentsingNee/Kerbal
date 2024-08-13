dca7190ebf506cfa332ec1e327659155842fec77
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-08-13 19:08:25 +0800

    [cmake] header_integration 支持所有子库

--- cmake/header_integration.cmake
+++ cmake/header_integration.cmake
@@ -9,27 +9,45 @@
 #   all rights reserved
 #
 
+foreach (
+        sublib IN ITEMS
+        kerbal
+        kerbal-omp
+        kerbal-parallel
+)
+    if (NOT TARGET ${sublib})
+        message(WARNING "header_integration ${sublib} ignored")
+        continue()
+    endif ()
 
-set(content_of_kerbal_header_integration
-    "
+    set(content_of_header_integration
+        "
 #ifndef LITTLE_ENDIAN
-#   define LITTLE_ENDIAN 4321
+#	define LITTLE_ENDIAN 4321
 #endif
 
 #ifndef BIG_ENDIAN
-#   define BIG_ENDIAN 1234
+#	define BIG_ENDIAN 1234
 #endif
 
 #ifndef BYTE_ORDER
-#   define BYTE_ORDER LITTLE_ENDIAN
+#	define BYTE_ORDER LITTLE_ENDIAN
 #endif
 
 "
-)
-foreach (header IN LISTS Kerbal_PUBLIC_HEADERS_RELATIVE_kerbal)
-    string(APPEND content_of_kerbal_header_integration "#include <${header}>\n")
+    )
+    foreach (header IN LISTS Kerbal_PUBLIC_HEADERS_RELATIVE_${sublib})
+        string(APPEND content_of_header_integration "#include <${header}>\n")
+    endforeach ()
+    set(header_integration_filename "${KERBAL_CONFIGURE_ASSETS_DIR}/header_integration/${sublib}.cpp")
+    file(WRITE "${header_integration_filename}" "${content_of_header_integration}")
+    add_library(
+            header_integration.${sublib}
+            OBJECT "${header_integration_filename}"
+    )
+    target_link_libraries(
+            header_integration.${sublib}
+            PRIVATE ${CMAKE_PROJECT_NAME}::${sublib}
+    )
+
 endforeach ()
-set(kerbal_header_integration_filename "${KERBAL_CONFIGURE_ASSETS_DIR}/kerbal_header_integration.cpp")
-file(WRITE "${kerbal_header_integration_filename}" "${content_of_kerbal_header_integration}")
-add_library(kerbal_header_integration OBJECT "${kerbal_header_integration_filename}")
-target_link_libraries(kerbal_header_integration PRIVATE ${CMAKE_PROJECT_NAME}::kerbal)
