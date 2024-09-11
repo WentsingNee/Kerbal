cff787a3b146a2a4163adb8a3a8ba30fb4ba64c6
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-09-11 18:00:31 +0800

    新增 rbset

---
+++ include/kerbal/container/rb_set.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file       rb_set.hpp
+ * @brief
+ * @date       2024-09-04
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_RB_SET_HPP
+#define KERBAL_CONTAINER_RB_SET_HPP
+
+#include <kerbal/container/rb_set/rb_set.fwd.hpp>
+#include <kerbal/container/rb_set/rb_set.decl.hpp>
+#include <kerbal/container/rb_set/rb_set.impl.hpp>
+
+#endif // KERBAL_CONTAINER_RB_SET_HPP
