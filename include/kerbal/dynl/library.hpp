d239f422780b9fe11b367cdf3596ac52805b8709
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-09-17 07:54:31 +0800

    [new-lib][dynl] 筹建 dynl 子库

---
+++ include/kerbal/dynl/library.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file       library.hpp
+ * @brief
+ * @date       2024-09-15
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_DYNL_LIBRARY_HPP
+#define KERBAL_DYNL_LIBRARY_HPP
+
+#include <kerbal/dynl/library/library.fwd.hpp>
+#include <kerbal/dynl/library/library.decl.hpp>
+#include <kerbal/dynl/library/library.impl.hpp>
+
+#endif // KERBAL_DYNL_LIBRARY_HPP
