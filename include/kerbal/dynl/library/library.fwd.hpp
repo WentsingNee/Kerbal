956f27e357f798e2e79cd11d961e216c9ac20302
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-09-17 07:54:31 +0800

    [new-lib][dynl] 筹建 dynl 子库

---
+++ include/kerbal/dynl/library/library.fwd.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file       library.fwd.hpp
+ * @brief
+ * @date       2024-09-15
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_DYNL_LIBRARY_LIBRARY_FWD_HPP
+#define KERBAL_DYNL_LIBRARY_LIBRARY_FWD_HPP
+
+namespace kerbal
+{
+
+	namespace dynl
+	{
+
+		class library;
+
+	} // namespace dynl
+
+} // namespace kerbal
+
+#endif // KERBAL_DYNL_LIBRARY_LIBRARY_FWD_HPP
---
+++ include/kerbal/dynl/library/library.impl.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file       library.impl.hpp
+ * @brief
+ * @date       2024-09-15
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_DYNL_LIBRARY_LIBRARY_IMPL_HPP
+#define KERBAL_DYNL_LIBRARY_LIBRARY_IMPL_HPP
+
+#include <kerbal/dynl/library/library.fwd.hpp>
+
+#include <kerbal/config/system.hpp>
+
+#if ( \
+	KERBAL_SYSTEM == KERBAL_SYSTEM_LINUX || \
+	KERBAL_SYSTEM == KERBAL_SYSTEM_APPLE || \
+	KERBAL_SYSTEM == KERBAL_SYSTEM_ANDROID \
+)
+#	include <kerbal/dynl/library/detail/unix/library.impl.unix.part.hpp>
+#endif
+
+#endif // KERBAL_DYNL_LIBRARY_LIBRARY_IMPL_HPP
