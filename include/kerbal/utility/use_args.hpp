6a6769f2d0a83e72da30fc6e427b617060c9828b
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-10-03 11:50:14 +0800

    use args

--- include/kerbal/utility/use_args.hpp
+++ include/kerbal/utility/use_args.hpp
@@ -1,7 +1,7 @@
 /**
  * @file       use_args.hpp
  * @brief
- * @date       2024-10-01
+ * @date       2024-10-03
  * @author     Peter
  * @copyright
  *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
@@ -12,68 +12,10 @@
 #ifndef KERBAL_UTILITY_USE_ARGS_HPP
 #define KERBAL_UTILITY_USE_ARGS_HPP
 
-#include <kerbal/compatibility/constexpr.hpp>
-#include <kerbal/type_traits/remove_cv.hpp>
-#include <kerbal/type_traits/remove_cvref.hpp>
-#include <kerbal/type_traits/integral_constant.hpp>
-#include <kerbal/utility/tuple.hpp>
-
-
-namespace kerbal
-{
-
-	namespace utility
-	{
-
-		template <typename ... Args>
-		class use_args_t :
-			public kerbal::utility::tuple<Args...>
-		{
-			private:
-				using super = kerbal::utility::tuple<Args...>;
-			public:
-				using super::super;
-		};
-
-		template <typename ... Args>
-		KERBAL_CONSTEXPR14
-		use_args_t<typename kerbal::type_traits::remove_cvref<Args>::type...>
-		use_args(Args && ... args)
-		{
-			return use_args_t<typename kerbal::type_traits::remove_cvref<Args>::type...>{
-				kerbal::utility::forward<Args>(args)...
-			};
-		}
-
-
-		namespace detail
-		{
-
-			template <typename T>
-			struct is_use_args_t_impl :
-				kerbal::type_traits::false_type
-			{
-			};
-
-			template <typename ... Args>
-			struct is_use_args_t_impl<kerbal::utility::use_args_t<Args...> > :
-				kerbal::type_traits::true_type
-			{
-			};
-
-		} // namespace detail
-
-
-		template <typename T>
-		struct is_use_args_t :
-			kerbal::utility::detail::is_use_args_t_impl<
-				typename kerbal::type_traits::remove_cv<T>::type
-			>
-		{
-		};
-
-	} // namespace utility
-
-} // namespace kerbal
+#if __cplusplus >= 201103L
+#	include <kerbal/utility/use_args/use_args.cxx11.part.hpp>
+#else
+#	include <kerbal/utility/use_args/use_args.cxx98.part.hpp>
+#endif
 
 #endif // KERBAL_UTILITY_USE_ARGS_HPP
