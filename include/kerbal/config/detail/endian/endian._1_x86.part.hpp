375c3e2c9105df41c00236f6bda6db868bdc585d
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-10-05 20:41:49 +0800

    [endian] 新增 endian

---
+++ include/kerbal/config/detail/endian/endian._1_x86.part.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file       endian.x86.part.hpp
+ * @brief
+ * @date       2024-10-05
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_ENDIAN__1_X86_PART_HPP
+#define KERBAL_ENDIAN__1_X86_PART_HPP
+
+#include <kerbal/config/detail/endian/endian.hpp>
+#include <kerbal/type_traits/integral_constant.hpp>
+
+
+#ifndef KERBAL_BYTE_ORDER
+#	define KERBAL_BYTE_ORDER kerbal::config::ENDIAN_LITTLE
+#endif
+
+#endif // KERBAL_ENDIAN__1_X86_PART_HPP
