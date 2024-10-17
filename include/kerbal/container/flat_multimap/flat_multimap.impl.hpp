09de5998125568bd5961c4f630bbf6d048533f4a
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-10-17 20:09:32 +0800

    [new-facility] 新增 flat_map flat_multimap

---
+++ include/kerbal/container/flat_multimap/flat_multimap.impl.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file       flat_multimap.impl.hpp
+ * @brief
+ * @date       2024-10-17
+ * @author     peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_CONTAINER_FLAT_MULTIMAP_FLAT_MULTIMAP_IMPL_HPP
+#define KERBAL_CONTAINER_FLAT_MULTIMAP_FLAT_MULTIMAP_IMPL_HPP
+
+#endif // KERBAL_CONTAINER_FLAT_MULTIMAP_FLAT_MULTIMAP_IMPL_HPP
