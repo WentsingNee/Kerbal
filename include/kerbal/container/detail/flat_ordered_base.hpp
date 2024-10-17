e31fadab4418a8c8a7f01a8b553f606c3edb4e95
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-10-18 00:57:58 +0800

    [coding-opt] 删除基于 lambda 的比较器

--- include/kerbal/container/detail/flat_ordered_base.hpp
+++ include/kerbal/container/detail/flat_ordered_base.hpp
@@ -88,18 +88,6 @@ namespace kerbal
 						kerbal::algorithm::sort(sequence.begin(), sequence.end(), this->value_comp());
 					}
 
-//#			if __cplusplus >= 201402L
-//
-//					KERBAL_CONSTEXPR14
-//					auto lower_bound_adapter() const
-//					{
-//						return [this](const_reference item, const key_type & key) -> bool {
-//							return this->key_comp()(Extract()(item), Extract()(key));
-//						};
-//					}
-//
-//#			else
-
 					friend struct lower_bound_kc_adapter;
 
 					struct lower_bound_kc_adapter
@@ -121,21 +109,6 @@ namespace kerbal
 							}
 					};
 
-//#			endif
-
-
-//#			if __cplusplus >= 201402L
-//
-//					KERBAL_CONSTEXPR14
-//					auto upper_bound_adapter() const
-//					{
-//						return [this](const key_type & key, const_reference item) -> bool {
-//							return this->key_comp()(key, Extract()(item));
-//						};
-//					}
-//
-//#			else
-
 					friend struct upper_bound_kc_adapter;
 
 					struct upper_bound_kc_adapter
@@ -156,10 +129,6 @@ namespace kerbal
 								return self->key_comp()(key, Extract()(item));
 							}
 					};
-
-
-//#			endif
-
 
 					friend struct equal_range_kc_adapter;
 
@@ -255,18 +224,6 @@ namespace kerbal
 						return key_compare_compress_helper::member();
 					}
 
-#			if __cplusplus >= 201402L
-
-					KERBAL_CONSTEXPR14
-					auto value_comp() const
-					{
-						return [this](const_reference lhs, const_reference rhs) -> bool {
-							return this->key_comp()(Extract()(lhs), Extract()(rhs));
-						};
-					}
-
-#			else
-
 					class value_compare
 					{
 							friend class flat_ordered_base;
@@ -290,8 +247,6 @@ namespace kerbal
 					{
 						return value_compare(this);
 					}
-
-#			endif
 
 				protected:
 
