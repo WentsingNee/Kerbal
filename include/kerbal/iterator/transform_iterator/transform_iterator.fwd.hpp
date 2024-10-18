092b86119365b5d19d62ea6976eaa2c4e815d7fd
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-10-18 18:01:41 +0800

    [new-facility] 新增 transform_iterator

---
+++ include/kerbal/iterator/transform_iterator/transform_iterator.fwd.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file       transform_iterator.fwd.hpp
+ * @brief
+ * @date       2024-10-18
+ * @author     peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_ITERATOR_TRANSFORM_ITERATOR_TRANSFORM_ITERATOR_FWD_HPP
+#define KERBAL_ITERATOR_TRANSFORM_ITERATOR_TRANSFORM_ITERATOR_FWD_HPP
+
+namespace kerbal
+{
+
+	namespace iterator
+	{
+
+		template <typename Iter, typename Transformer>
+		class transform_iterator;
+
+		namespace detail
+		{
+
+			template <typename InputIterator, typename Transformer, typename IteratorTag>
+			class transform_iterator_impl;
+
+		} // namespace detail
+
+	} // namespace iterator
+
+} // namespace kerbal
+
+#endif // KERBAL_ITERATOR_TRANSFORM_ITERATOR_TRANSFORM_ITERATOR_FWD_HPP
