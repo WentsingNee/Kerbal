cd04e6d68c1b69663e24d3faa319244ed9d4c3d5
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-11-22 00:08:55 +0800

    sha1 final opt

--- include/kerbal/hash/sha1/sha1.impl.hpp
+++ include/kerbal/hash/sha1/sha1.impl.hpp
@@ -151,20 +151,23 @@ namespace kerbal
 			}; /* Endian independent */
 
 			{
-				uint8_t p[64] = {0200};
-				uint8_t const * kp = p;
-				this->update(kp, kp + 1);
+				{
+					uint8_t const p[1] = {0200};
+					this->update(p, p + 1);
+				}
 				// 504 = 0b111111000
 				// 448 = 0b111000000
 				std::size_t padding = this->count[0] & 504;
-				p[0] = 0;
-				if (padding > 448) {
-					padding = 448 + (512 - padding);
-				} else {
-					padding = 448 - padding;
+				{
+					uint8_t const p[64] = {};
+					if (padding > 448) {
+						padding = 448 + (512 - padding);
+					} else {
+						padding = 448 - padding;
+					}
+					padding /= 8;
+					this->update(p, p + padding);
 				}
-				padding /= 8;
-				this->update(kp, kp + padding);
 			}
 
 			this->update(final_count, final_count + 8); /* Should cause a SHA1Transform() */
