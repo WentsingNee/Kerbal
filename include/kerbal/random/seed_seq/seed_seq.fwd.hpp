b533be0a282961d9690f5ded28ad9e8e33f3ad95
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2025-01-10 23:17:50 +0800

    [new-facility] 新增 seed_seq

---
+++ include/kerbal/random/seed_seq/seed_seq.fwd.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file       seed_seq.fwd.hpp
+ * @brief
+ * @date       2025-01-09
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_RANDOM_SEED_SEQ_SEED_SEQ_FWD_HPP
+#define KERBAL_RANDOM_SEED_SEQ_SEED_SEQ_FWD_HPP
+
+namespace kerbal
+{
+	namespace random
+	{
+
+		class seed_seq;
+
+	} // namespace random
+
+} // namespace kerbal
+
+#endif // KERBAL_RANDOM_SEED_SEQ_SEED_SEQ_FWD_HPP
---
+++ include/kerbal/random/seed_seq/seed_seq.impl.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file       seed_seq.impl.hpp
+ * @brief
+ * @date       2025-01-09
+ * @author     Peter
+ * @copyright
+ *      Peter of [ThinkSpirit Laboratory](http://thinkspirit.org/)
+ *   of [Nanjing University of Information Science & Technology](http://www.nuist.edu.cn/)
+ *   all rights reserved
+ */
+
+#ifndef KERBAL_RANDOM_SEED_SEQ_SEED_SEQ_IMPL_HPP
+#define KERBAL_RANDOM_SEED_SEQ_SEED_SEQ_IMPL_HPP
+
+#include <kerbal/random/seed_seq/seed_seq.decl.hpp>
+
+#endif // KERBAL_RANDOM_SEED_SEQ_SEED_SEQ_IMPL_HPP
