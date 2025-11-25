c7a53d09c37f84e8093179201869d069075794a6
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2025-11-25 18:45:39 +0800

    [editorial-change] 编辑性修改

--- include/kerbal/container/detail/vector_base/vector_base.impl.hpp
+++ include/kerbal/container/detail/vector_base/vector_base.impl.hpp
@@ -2159,6 +2159,7 @@ namespace kerbal
  \
 				this->k_emplace_back_transfer_ele(alloc, new_buffer, new_capacity); \
 			} \
+ \
 			template <typename T> \
 			template < \
 				typename Allocator \
